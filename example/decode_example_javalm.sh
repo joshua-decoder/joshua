2d8f79a3516a6857d0d95e79e49fb309eb3ff12e
zhifeili <zhifeili@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2008-10-31 05:04:33 +0000

    modify javalm config
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@210 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- example/decode_example_javalm.sh
+++ example/decode_example_javalm.sh
@@ -1,2 +1,2 @@
 cd ..
-java -classpath $CLASS_PATH:./bin -Xmx2000m -Xms2000m joshua.decoder.Decoder example/example.config.javalm example/example.test.in example/example.nbest.javalm.out
+java -classpath $CLASS_PATH:./bin -Xmx2000m -Xms2000m joshua.decoder.JoshuaDecoder example/example.config.javalm example/example.test.in example/example.nbest.javalm.out
--- example/decode_example_srilm.sh
+++ example/decode_example_srilm.sh
@@ -1,2 +1,2 @@
 cd ..
-java  -classpath $CLASS_PATH:./bin -Xmx2000m -Xms2000m joshua.decoder.Decoder example/example.config.srilm example/example.test.in example/example.nbest.srilm.out
+java  -classpath $CLASS_PATH:./bin -Xmx2000m -Xms2000m joshua.decoder.JoshuaDecoder example/example.config.srilm example/example.test.in example/example.nbest.srilm.out
