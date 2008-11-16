c3755f15546cbd56935882bb6a179df3a2914f83
zhifeili <zhifeili@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2008-11-16 04:51:23 +0000

    add debug info
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@313 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- src/joshua/decoder/ff/PhraseModelFF.java
+++ src/joshua/decoder/ff/PhraseModelFF.java
@@ -42,7 +42,7 @@ public final class PhraseModelFF extends DefaultStatelessFF  {
 			if (this.columnIndex < rule.feat_scores.length) {
 				return rule.feat_scores[this.columnIndex];
 			} else {
-				System.out.println("In PhraseModelFF: columnIndex is not right, model columnIndex: " + columnIndex + "; max colum in rule: rule.feat_scores.length");
+				System.out.println("In PhraseModelFF: columnIndex is not right, model columnIndex: " + columnIndex + "; rule is: " + rule.toString());
 				System.exit(0);
 				return 0.0;
 			}
