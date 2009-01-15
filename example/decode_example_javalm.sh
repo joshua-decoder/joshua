a69c05063e2fab0b927d2393a5fbd46760c5ae21
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-01-15 16:27:58 +0000

    Adding more docs for examples
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@408 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- example/decode_example_javalm.sh
+++ example/decode_example_javalm.sh
@@ -1,2 +1,3 @@
-cd ..
-java -classpath $CLASS_PATH:$JOSHUA_HOME/bin -Xmx1000m -Xms1000m joshua.decoder.JoshuaDecoder example/example.config.javalm example/example.test.in example/example.nbest.javalm.out
+#!/bin/bash
+
+java -classpath $CLASSPATH:bin -Xmx1000m -Xms1000m joshua.decoder.JoshuaDecoder example/example.config.javalm example/example.test.in example/example.nbest.javalm.out
