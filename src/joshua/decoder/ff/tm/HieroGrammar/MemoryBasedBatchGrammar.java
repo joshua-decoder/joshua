c298b0491efcce0d074afe705dcfb2fc2645e1cf
zhifeili <zhifeili@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-03-11 03:22:10 +0000

    Add some comments
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@568 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- src/joshua/decoder/ff/tm/HieroGrammar/MemoryBasedBatchGrammar.java
+++ src/joshua/decoder/ff/tm/HieroGrammar/MemoryBasedBatchGrammar.java
@@ -1,3 +1,20 @@
+/* This file is part of the Joshua Machine Translation System.
+ * 
+ * Joshua is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
 package joshua.decoder.ff.tm.HieroGrammar;
 
 import java.io.BufferedReader;
@@ -16,6 +33,14 @@ import joshua.decoder.ff.tm.Rule;
 import joshua.decoder.ff.tm.BilingualRule;
 import joshua.decoder.ff.tm.Trie;
 import joshua.util.FileUtility;
+
+
+/**
+ * this class implements MemoryBasedBatchGrammar
+ * 
+ * @author Zhifei Li, <zhifei.work@gmail.com>
+ * @version $LastChangedDate: 2009-03-09 12:52:29 -0400 (星期一, 09 三月 2009) $
+ */
 
 public class MemoryBasedBatchGrammar  extends BatchGrammar {
 	/*TMGrammar is composed by Trie nodes
