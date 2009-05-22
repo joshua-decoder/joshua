ffa8361ba6e4c3251a9ca0119f325f052b448564
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-05-22 19:38:50 +0000

    Renamed src folders for tools in such a way that they can be added as source folders in Eclipse.
    Eclipse does not allow multiple source folders to have the same name, so having each tool folder have its own folder called src doesn't work.
    
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@844 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- tools/tree_visualizer/src_tree_visualizer/joshua/ui/tree_visualizer/DerivationViewer.java
+++ tools/tree_visualizer/src_tree_visualizer/joshua/ui/tree_visualizer/DerivationViewer.java
@@ -15,7 +15,7 @@
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
  */
-package joshua.decoder;
+package joshua.ui.tree_visualizer;
 
 import java.awt.Dimension;
 import java.awt.Paint;
