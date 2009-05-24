16b576c7035fd4e0b2058263a99f8d823c0a63c6
jweese <jweese@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-05-24 15:59:41 +0000

    Forgot to update the Node class for derivation trees. Should be okay now.
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@853 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- tools/tree_visualizer/src_tree_visualizer/joshua/ui/tree_visualizer/Node.java
+++ tools/tree_visualizer/src_tree_visualizer/joshua/ui/tree_visualizer/Node.java
@@ -1,48 +1,42 @@
-/* This file is part of the Joshua Machine Translation System.
- *
- * Joshua is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
- */
-package joshua.ui.tree_visualizer;
-
+// a wrapper class
+// so that we can have nodes with the same labels without them
+// being the same node.
+// JUNG uses equals() comparison rather than pointer comparison to see
+// if two nodes are the same.
 class Node {
 	private String name;
 	private int sourceStart;
 	private int sourceEnd;
 	private boolean isSource;
+	private Node counterpart = null;
 
 	public static final String DELIM = "[\\{\\}\\-]";
 
 	public Node(String name, boolean isSource) {
 		this.name = name;
 		this.isSource = isSource;
+		if (name.endsWith("}")) {
+			String [] split = name.split(DELIM);
+			this.name = split[0];
+			sourceStart = Integer.parseInt(split[1]);
+			sourceEnd = Integer.parseInt(split[2]);
+		}
 	}
 
 	public Node(String name, Node parent, boolean isSource)
 	{
 		this.name = name;
 		this.isSource = isSource;
-		String [] split;
 		if (name.endsWith("}")) {
-			split = name.split(DELIM);
+			String [] split = name.split(DELIM);
+			this.name = split[0];
+			sourceStart = Integer.parseInt(split[1]);
+			sourceEnd = Integer.parseInt(split[2]);
 		}
 		else {
-			split = parent.toString().split(DELIM);
+			sourceStart = parent.sourceStart();
+			sourceEnd = parent.sourceEnd();
 		}
-		sourceStart = Integer.parseInt(split[1]);
-		sourceEnd = Integer.parseInt(split[2]);
 	}
 
 	public String toString()
@@ -81,5 +75,15 @@ class Node {
 			ret += " " + toks[i];
 		}
 		return ret;
+	}
+
+	public void setCounterpart(Node n)
+	{
+		counterpart = n;
+	}
+
+	public Node getCounterpart()
+	{
+		return counterpart;
 	}
 }
