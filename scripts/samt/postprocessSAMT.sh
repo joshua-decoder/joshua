20586b4d6174460d73a60d9cd4844ddfa92a9684
jganitkevitch <jganitkevitch@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-06-05 00:58:04 +0000

    Added SAMT output postprocessing script.
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@911 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

---
+++ scripts/samt/postprocessSAMT.sh
@@ -0,0 +1,17 @@
+
+if [ $# -ne 3 ]
+then
+  echo "Usage: `basename $0` samt.merged.gz samt.tm.gz samt.glue.gz"
+  exit 2
+fi
+
+if [ ! -r $1 ]
+then
+  echo "Error: file $1 does not exist or is not readable."
+  exit 3
+fi
+
+zgrep -v COUNT $1 | gzip > $2
+zgrep COUNT $1 | awk 'BEGIN { FS="#" } ; { print $3 "#@1#@GOAL#1 0 0 0 0 0 0 0"; print "@GOAL " $3 "#@1 @2#@GOAL#1 0 0 0 0.434294482 0 0 0" }' | gzip > $3
+
+
