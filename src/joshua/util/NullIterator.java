8d3ee95ba5b08c8ecdff5dee587fc7103c9fd110
winterkoninkje <winterkoninkje@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-06-10 02:39:50 +0000

    Minor documentation changes to NullIterator.
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@920 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- src/joshua/util/NullIterator.java
+++ src/joshua/util/NullIterator.java
@@ -22,8 +22,8 @@ import java.util.NoSuchElementException;
 
 
 /**
- * This class provides a null-object Iterator. That is, an empty
- * iterator.
+ * This class provides a null-object Iterator. That is, an iterator
+ * over an empty collection.
  *
  * @author wren ng thornton <wren@users.sourceforge.net>
  * @version $LastChangedDate: 2009-03-26 15:06:57 -0400 (Thu, 26 Mar 2009) $
@@ -31,10 +31,15 @@ import java.util.NoSuchElementException;
 public class NullIterator<E> implements Iterable<E>, Iterator<E> {
 	
 //===============================================================
-// Iterable -- because sometimes Java can be very stupid
+// Iterable -- for foreach loops, because sometimes Java can be very stupid
 //===============================================================
 	
-	/** Return this object. */
+	/**
+	 * Return self as an iterator. We restrict the return type
+	 * because some code is written to accept both Iterable and
+	 * Iterator, and the fact that we are both confuses Java.
+	 * So this is just an upcast, but more succinct to type.
+	 */
 	public Iterator<E> iterator() { return this; }
 	
 	
