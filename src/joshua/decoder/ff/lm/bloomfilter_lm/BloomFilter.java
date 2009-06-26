893c46e9718b13b4f93bb7c37776a18d1f4da24a
jweese <jweese@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-06-26 16:02:03 +0000

    Code cleanup. Added javadocs to sources in joshua.decoder.ff.lm.bloomfilter_lm
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@977 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- src/joshua/decoder/ff/lm/bloomfilter_lm/BloomFilter.java
+++ src/joshua/decoder/ff/lm/bloomfilter_lm/BloomFilter.java
@@ -24,20 +24,74 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.IOException;
 
+/**
+ * A Bloom filter: a lossy data structure for set representation. A Bloom
+ * filter consists of a bit set and a set of hash functions. A Bloom filter has
+ * two operations: add and query. We can add an object to a Bloom filter to
+ * indicate that it should be considered part of the set that the Bloom filter
+ * represents. We can query the Bloom filter to see if a given object is
+ * considered part of its set.
+ * <p>
+ * An object is added by sending it through a number of hash functions, each
+ * of which returns an index into the bit set. The bit at each of the indices
+ * is flipped on. We can query for an abject by sending it through the same
+ * hash functions. Then we look the bit at each index that was returned by a
+ * hash function. If any of the bits is unset, we know that the object is not
+ * in the Bloom filter (for otherwise all the bits should have already been
+ * set). If all the bits are set, we assume that the object is present in the
+ * Bloom filter.
+ * <p>
+ * We cannot know for sure that an object is in the bloom filter just because
+ * all its bits were set. There may be many collisions in the hash space, and
+ * all the bits for some object might be set by chance, rather than by adding
+ * that particular object.
+ * <p>
+ * The advantage of a Bloom filter is that its set representation can be stored
+ * in a significantly smaller space than information-theoretic lossless lower
+ * bounds. The price we pay for this is a certain amount of error in the query
+ * function. One nice feature of the Bloom filter is that its error is
+ * one-sided. This means that while the query function may return false
+ * positives (saying an object is present when it really isn't), it can never
+ * return false negatives (saying that an object is not present when it was
+ * already added.
+ */
 public class BloomFilter implements Externalizable {
+	/**
+	 * The main bit set of the Bloom filter.
+	 */
 	private BitSet bitSet;
+
+	/**
+	 * The number of objects expected to be stored in the Bloom filter.
+	 * The optimal number of hash functions depends on this number.
+	 */
 	int expectedNumberOfObjects;
-	//private BigInteger bigPrime;
-	//private BigInteger filterSize;
+
+	/**
+	 * A prime number that should be bigger than the size of the bit set.
+	 */
 	long bigPrime;
+
+	/**
+	 * The size of the bit set, in bits.
+	 */
 	int filterSize;
+
+	/**
+	 * A random number generator for building hash functions.
+	 */
 	transient private Random RANDOM = new Random();
 
-
+	/**
+	 * Builds an empty Bloom filter, ready to build hash functions
+	 * and store objects.
+	 *
+	 * @param filterSize the size of Bloom filter to make, in bits
+	 * @param expectedNumberOfObjects the number of objects expected
+	 *                                to be stored in the Bloom filter
+	 */
 	public BloomFilter(int filterSize, int expectedNumberOfObjects) {
 		bitSet = new BitSet(filterSize);
-		//this.filterSize = convertIntToBigInteger(filterSize);
-		//this.filterSize = BigInteger.valueOf((long) filterSize);
 		this.filterSize = filterSize;
 		this.expectedNumberOfObjects = expectedNumberOfObjects;
 		bigPrime = getPrimeLargerThan(filterSize);
@@ -46,6 +100,10 @@ public class BloomFilter implements Externalizable {
 	/**
 	 * Adds an item (represented by an integer) to the bloom
 	 * filter.
+	 *
+	 * @param objectToAdd the object to add
+	 * @param hashFunctions an array of pairs of long, representing the
+	 *                      hash functions to be used on the object
 	 */
 	public void add(int objectToAdd, long [][] hashFunctions) {
 		for (long [] h : hashFunctions) {
@@ -63,7 +121,14 @@ public class BloomFilter implements Externalizable {
 
 	/**
 	 * Determines whether an item (represented by an integer)
-	 * is present in the bloom filter. Returns its value.
+	 * is present in the bloom filter.
+	 *
+	 * @param objectToQuery the object we want to query for membership
+	 * @param hashFunctions an array of pairs of long, representing the
+	 *                      hash functions to be used
+	 *
+	 * @return true if the objects is assumed to be present in the Bloom
+	 *         filter, false if it is definitely not present
 	 */
 	public boolean query(int objectToQuery, long [][] hashFunctions) {
 		for (long [] h : hashFunctions) {
@@ -83,6 +148,12 @@ public class BloomFilter implements Externalizable {
 		return true;
 	}
 
+	/**
+	 * Builds an array of pairs of long that can be used as hash functions
+	 * for this Bloom filter.
+	 *
+	 * @return an array of pairs of long suitable for use as hash functions
+	 */
 	public long [][] initializeHashFunctions() {
 		int numberOfHashFunctions;
 		int bigPrimeInt = (int) bigPrime;
@@ -90,38 +161,39 @@ public class BloomFilter implements Externalizable {
 		if (numberOfHashFunctions == 0) numberOfHashFunctions = 1;
 		long [][] hashFunctions = new long[numberOfHashFunctions][2];
 		for (long [] h : hashFunctions) {
-			//h[0] = convertIntToBigInteger(RANDOM.nextInt(bigPrimeInt) + 1);
-			//h[1] = convertIntToBigInteger(RANDOM.nextInt(bigPrimeInt) + 1);
 			h[0] = (long) RANDOM.nextInt(bigPrimeInt) + 1;
 			h[1] = (long) RANDOM.nextInt(bigPrimeInt) + 1;
 		}
 		return hashFunctions;
 	}
 
+	/**
+	 * Determines which bit of the bit set should be either set, for add
+	 * operations, or checked, for query operations.
+	 *
+	 * @param h a length-2 array of long used as a hash function
+	 * @param objectToHash the object of interest
+	 *
+	 * @return an index into the bit set of the Bloom filter
+	 */
 	private int hash(long [] h, long objectToHash) {
-		//BigInteger result;
-		//BigInteger bigObjectToHash;
-		//bigObjectToHash = convertIntToBigInteger(objectToHash);
-		//bigObjectToHash = BigInteger.valueOf(objectToHash);
-		//result = h[1].add(h[0].multiply(bigObjectToHash));
-		//return result.mod(bigPrime).mod(filterSize).intValue();
-//		System.out.println("bigPrime: " + bigPrime);
-//		System.out.println("object: " + objectToHash);
-//		System.out.println("Integer.MAX_VALUE: " + Integer.MAX_VALUE);
-//		System.out.println("h[0]: " + h[0]);
-//		System.out.println("h[1]: " + h[1]);
 		long obj = (objectToHash < Integer.MAX_VALUE) ? objectToHash : objectToHash - bigPrime;
 		long h0 = h[0];
 		long h1 = (h[1] < (Long.MAX_VALUE / 2)) ? h[1] : h[1] - bigPrime;
-//		System.out.println("object: " + objectToHash);
-//		System.out.println("h[0]: " + h[0]);
-//		System.out.println("h[1]: " + h[1]);
-//		System.out.println("product: " + obj*h0);
 		long ret = (obj * h0) % bigPrime;
 		ret = (ret < (Long.MAX_VALUE / 2)) ? ret : ret - bigPrime;
 		return (int) (((ret + h1) % bigPrime) % (long) filterSize);
 	}
 
+	/**
+	 * Finds a prime number that is larger than the given number.
+	 * This is used to find bigPrime, a prime that has to be larger than
+	 * the size of the Bloom filter.
+	 *
+	 * @param n an integer
+	 *
+	 * @return a prime number larger than n
+	 */
 	private long getPrimeLargerThan(int n) {
 		BigInteger ret;
 		BigInteger maxLong = BigInteger.valueOf(Long.MAX_VALUE);
@@ -130,13 +202,6 @@ public class BloomFilter implements Externalizable {
 			ret = BigInteger.probablePrime(numBits, RANDOM);
 		} while (ret.compareTo(maxLong) > 1);
 		return ret.longValue();
-	}
-
-	//TODO This method is never used - perhaps it should be removed
-	@SuppressWarnings("unused")
-	private BigInteger convertIntToBigInteger(int n)
-	{
-		return new BigInteger(Integer.valueOf(n).toString());
 	}
 
 	/*
--- src/joshua/decoder/ff/lm/bloomfilter_lm/BloomFilterLanguageModel.java
+++ src/joshua/decoder/ff/lm/bloomfilter_lm/BloomFilterLanguageModel.java
@@ -42,36 +42,102 @@ import joshua.decoder.ff.lm.bloomfilter_lm.BloomFilter;
 import joshua.corpus.vocab.SymbolTable;
 import joshua.corpus.vocab.Vocabulary;
 
-
+/**
+ * An n-gram language model with linearly-interpolated Witten-Bell smoothing,
+ * using a Bloom filter as its main data structure. A Bloom filter is a lossy
+ * data structure that can be used to test for set membership.
+ */
 public class BloomFilterLanguageModel extends AbstractLM implements Externalizable {
+	/**
+	 * An initial value used for hashing n-grams so that they can be stored
+	 * in a bloom filter.
+	 */
 	public static final int HASH_SEED = 17;
+
+	/**
+	 * Another value used in the process of hashing n-grams.
+	 */
 	public static final int HASH_OFFSET = 37;
 
+	/**
+	 * The maximum score that a language model feature function can return
+	 * to the Joshua decoder.
+	 */
 	public static final double MAX_SCORE = 100.0;
 
+	/**
+	 * The logger for this class.
+	 */
 	public static final Logger logger = 
 		Logger.getLogger(BloomFilterLanguageModel.class.getName());
 
+	/**
+	 * A map from string to integer containing all types in the language
+	 * model. The Bloom filter LM needs to maintain an internal vocabulary
+	 * because the hashed values of n-grams are based on a vocabulary that
+	 * is populated when the LM is first constructed. The symbol table from
+	 * a later run of the Joshua decoder is not guaranteed to have the same
+	 * mappings, which would make it impossible to query the bloom filter
+	 * in a meaningful way.
+	 */
 	private SymbolTable vocabulary;
-	private BloomFilter bf;
-	private double quantizationBase;
-	private double numTokens;	// log number of tokens seen in training corpus
 
-	// hash functions for storing or retreiving ngram counts in the
-	// bloom filter
+	/**
+	 * The Bloom filter data structure itself.
+	 */
+	private BloomFilter bf;
+
+	/**
+	 * The base of the logarithm used to quantize n-gram counts. N-gram
+	 * counts are quantized logarithmically to reduce the number of times
+	 * we need to query the Bloom filter.
+	 */
+	private double quantizationBase;
+
+	/**
+	 * Natural log of the number of tokens seen in the training corpus.
+	 */
+	private double numTokens;
+
+	/**
+	 * An array of pairs of long, used as hash functions for storing or
+	 * retreiving the count of an n-gram in the Bloom filter.
+	 */
 	private long [][] countFuncs;
-	// hash functions for storing or retrieving the number of distinct
-	// types observed after an ngram
+	/**
+	 * An array of pairs of long, used as hash functions for storing or
+	 * retreiving the number of distinct types observed after an n-gram.
+	 */
 	private long [][] typesFuncs;
 
-	// translation model to language model mapping
-	transient private int [] TMtoLMMapping;
-	transient private double p0; // probability of the empty ngram
-	transient private double lambda0; // interpolation constant between models of order 1 and 0
+	/**
+	 * The smoothed probability of an unseen n-gram. This is also the
+	 * probability of any n-gram under the zeroth-order model.
+	 */
+	transient private double p0;
+
+	/**
+	 * The interpolation constant between Witten-Bell models of order zero
+	 * and one. Stored in a field because it can be calculated ahead of
+	 * time; it doesn't depend on the particular n-gram.
+	 */
+	transient private double lambda0;
+
+	/**
+	 * The maximum possible quantized count of any n-gram stored in the 
+	 * Bloom filter. Used as an upper bound on the count that could be
+	 * returned when querying the Bloom filter.
+	 */
 	transient private int maxQ;	// max quantized count
 
-	/*
-	 * constructor.
+	/**
+	 * Constructor called from the Joshua decoder. This constructor assumes
+	 * that the LM has already been built, and takes the name of the file
+	 * where the LM is stored.
+	 *
+	 * @param symbols a symbol table used globally by the Joshua decoder
+	 * @param order the order of the language model
+	 * @param filename path to the file where the language model is stored
 	 */
 	public BloomFilterLanguageModel(SymbolTable symbols, int order, String filename) throws IOException {
 		super(symbols, order);
@@ -83,29 +149,24 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 			throw ioe;
 		}
 
-		TMtoLMMapping = createTMtoLMMapping();
 		int vocabSize = vocabulary.size();
-		System.err.println("BFLM: vocabSize: " + vocabSize);
-//		System.err.println("BFLM: TM symbol table size: " + symbolTable.size());
 		p0 = -Math.log(vocabSize + 1);
 		double oneMinusLambda0 = numTokens - logAdd(Math.log(vocabSize), numTokens);
 		p0 += oneMinusLambda0;
-		System.err.println("BFLM: p0: " + p0);
 		lambda0 = Math.log(vocabSize) - logAdd(Math.log(vocabSize), numTokens);
-		System.err.println("BFLM: lambda0: " + lambda0);
 		maxQ = quantize((long) Math.exp(numTokens));
-		System.err.println("BFLM: maxQ: " + maxQ);
-		/*
-		for (String w : translationModelSymbols.getWords()) {
-			if (vocabulary.getID(w) != vocabulary.getUnknownWordID())
-				System.err.println("BFLM: word " + w + "found.");
-			int [] tmp = { vocabulary.getID(w) };
-			if (bf.query(hashNgram(tmp, 0, 1, 1), countFuncs))
-				System.err.println("BFLM: found " + w);
-		}
-		*/
 	}
 	
+	/**
+	 * Constructor to be used by the main function. This constructor is
+	 * used to build a new language model from scratch. An LM should be
+	 * built with the main function before using it in the Joshua decoder.
+	 *
+	 * @param filename path to the file of training corpus statistics
+	 * @param order the order of the language model
+	 * @param size the size of the Bloom filter, in bits
+	 * @param base a double. The base of the logarithm for quantization.
+	 */
 	private BloomFilterLanguageModel(String filename, int order, int size, double base) {
 		super(null, order);
 		quantizationBase = base;
@@ -113,16 +174,8 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		populateBloomFilter(size, filename);
 	}
 
-	private int [] createTMtoLMMapping() {
-		int [] map = new int[symbolTable.getHighestID()];
-		for (String word : symbolTable.getWords()) {
-			map[symbolTable.getID(word)] = vocabulary.getID(word);
-		}
-		return map;
-	}
-	
-	/*
-	 * calculates the linearly-interpolated witten-bell probability
+	/**
+	 * calculates the linearly-interpolated Witten-Bell probability
 	 * for a given ngram.
 	 * this is calculated as:
 	 * p(w|h) = pML(w|h)L(h) - (1 - L(h))p(w|h')
@@ -135,26 +188,27 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 	 * L(h) = s(h) / s(h) + c(h) where s(.) is the observed number of
 	 * distinct types after h, and c is the observed number of counts
 	 * of h in the training corpus.
-	 *
+	 * <p>
 	 * in fact this model calculates the probability starting from the
 	 * lowest order and working its way up, to take advantage of the one-
 	 * sided error rate inherent in using a bloom filter data structure.
 	 *
+	 * @param ngram the ngram whose probability is to be calculated
+	 * @param ngramOrder the order of the ngram.
+	 *
+	 * @return the linearly-interpolated Witten-Bell smoothed probability
+	 * of an ngram
 	 */
 	private double wittenBell(int [] ngram, int ngramOrder) {
 		int end = ngram.length;
 		double p = p0; // current calculated probability
 		// note that p0 and lambda0 are independent of the given
 		// ngram so they are calculated ahead of time.
-		//p *= (1 - lambda0);
-		//p += logAdd(0, -lambda0);
 		int MAX_QCOUNT = getCount(ngram, ngram.length-1, ngram.length, maxQ);
-//		System.err.println("word: " + MAX_QCOUNT);
 		if (MAX_QCOUNT == 0) // OOV!
 			return p;
 		double pML = Math.log(unQuantize(MAX_QCOUNT)) - numTokens;
 
-//		System.err.println("pML: " + pML);
 		//p += lambda0 * pML;
 		p = logAdd(p, (lambda0 + pML));
 		if (ngram.length == 1) { // if it's a unigram, we're done
@@ -162,13 +216,8 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		}
 		// otherwise we calculate the linear interpolation
 		// with higher order models.
-//		System.err.println("BFLM: end: " + end);
-//		System.err.println("BFLM: ngramOrder: " + ngramOrder);
 		for (int i = end - 2; i >= end - ngramOrder && i >= 0; i--) {
-			//System.err.println("word: " + word);
-			//System.err.println("history: " + history);
 			int historyCnt = getCount(ngram, i, end, MAX_QCOUNT);
-//			System.err.println("history count: " + historyCnt);
 			// if the count for the history is zero, all higher
 			// terms in the interpolation must be zero, so we
 			// are done here.
@@ -183,14 +232,10 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 			double lambda = Math.log(HTA) - Math.log(HTA + HC);
 			double oneMinusLambda = Math.log(HC) - Math.log(HTA + HC);
 			// p *= 1 - lambda
-			p += oneMinusLambda; //logAdd(0, -lambda);
+			p += oneMinusLambda;
 			int wordCount = getCount(ngram, i+1, end, historyTypesAfter);
 			double WC = unQuantize(wordCount);
-//			System.err.println("HTA: " + HTA);
-//			System.err.println("HC: " + HC);
-//			System.err.println("WC: " + WC);
-//			System.err.println("pML(word) " + (WC/HC));
-			//p += (lambda * (WC / HC)); // p_ML(w|h)
+			//p += lambda * p_ML(w|h)
 			if (WC == 0)
 				return p;
 			p = logAdd(p, lambda + Math.log(WC) - Math.log(HC));
@@ -199,8 +244,19 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return p;
 	}
 	
-	/*
-	 * this corresponds roughly to algorithm 2 in talbot+osborne.
+	/**
+	 * Retrieve the count of a ngram from the Bloom filter. That is, how
+	 * many times did we see this ngram in the training corpus?
+	 * This corresponds roughly to algorithm 2 in Talbot and Osborne's
+	 * "Tera-Scale LMs on the Cheap."
+	 *
+	 * @param ngram array containing the ngram as a sub-array
+	 * @param start the index of the first word of the ngram
+	 * @param end the index after the last word of the ngram
+	 * @param qcount the maximum possible count to be returned
+	 *
+	 * @return the number of times the ngram was seen in the training
+	 * corpus, quantized
 	 */
 	private int getCount(int [] ngram, int start, int end, int qcount) {
 		for (int i = 1; i <= qcount; i++) {
@@ -212,11 +268,21 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return qcount;
 	}
 	
-	/*
-	 * this is another version of algorithm 2. As noted in the paper,
+	/**
+	 * Retrieve the number of distinct types that follow an ngram in the
+	 * training corpus.
+	 *
+	 * This is another version of algorithm 2. As noted in the paper,
 	 * we have different algorithms for getting ngram counts versus
 	 * suffix counts because c(x) = 1 is a proxy item for s(x) = 1
 	 *
+	 * @param ngram an array the contains the ngram as a sub-array
+	 * @param start the index of the first word of the ngram
+	 * @param end the index after the last word of the ngram
+	 * @param qcount the maximum possible return value
+	 *
+	 * @return the number of distinct types observed to follow an ngram
+	 * in the training corpus, quantized
 	 */
 	private int getTypesAfter(int [] ngram, int start, int end, int qcount) {
 		// first we check c(x) >= 1
@@ -234,15 +300,25 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return qcount;
 	}
 	
-	/*
-	 * logarithmic quantization
+	/**
+	 * Logarithmically quantizes raw counts.
+	 * The quantization scheme is described in Talbot and Osborne's paper
+	 * "Tera-Scale LMs on the Cheap."
+	 *
+	 * @param x long giving the raw count to be quantized
+	 *
+	 * @return the quantized count
 	 */
 	private int quantize(long x) {
 		return 1 + (int) Math.floor(Math.log(x)/Math.log(quantizationBase));
 	}
 	
-	/*
-	 * returns the true expected value of a given quantized value
+	/**
+	 * Unquantizes a quantized count.
+	 * 
+	 * @param x the quantized count
+	 *
+	 * @return the expected raw value of the quantized count
 	 */
 	private double unQuantize(int x) {
 		if (x == 0) {
@@ -252,9 +328,18 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		}
 	}
 	
-	/*
-	 * this is adapted directly from AbstractPhrase.hashCode() elsewhere
-	 * in the joshua code base.
+	/**
+	 * Converts an n-gram and a count into a value that can be stored into
+	 * a Bloom filter. This is adapted directly from
+	 * <code>AbstractPhrase.hashCode()</code> elsewhere in the Joshua
+	 * code base.
+	 *
+	 * @param ngram an array containing the ngram as a sub-array
+	 * @param start the index of the first word of the ngram
+	 * @param end the index after the last word of the ngram
+	 * @param val the count of the ngram
+	 *
+	 * @return a value suitable to be stored in a Bloom filter
 	 */
 	private int hashNgram(int [] ngram, int start, int end, int val) {
 		int result = HASH_OFFSET*HASH_SEED + val;
@@ -263,6 +348,14 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return result;
 	}
 	
+	/**
+	 * Adds two numbers that are in the log domain, avoiding underflow.
+	 *
+	 * @param x one summand
+	 * @param y the other summand
+	 *
+	 * @return the log of the sum of the exponent of the two numbers.
+	 */
 	private static double logAdd(double x, double y) {
 		if (y <= x) {
 			return x + Math.log1p(Math.exp(y - x));
@@ -271,6 +364,11 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		}
 	}
 	
+	/**
+	 * Builds a language model and stores it in a file.
+	 *
+	 * @param argv command-line arguments
+	 */
 	public static void main(String [] argv) {
 		if (argv.length < 5) {
 			System.err.println("usage: BloomFilterLanguageModel <statistics file> <order> <size> <quantization base> <output file>");
@@ -311,6 +409,18 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return 0;
 	}
 	
+	/**
+	 * Adds ngram counts and counts of distinct types after ngrams, read
+	 * from a file, to the Bloom filter.
+	 * <p>
+	 * The file format should look like this:
+	 * ngram1	count	types-after
+	 * ngram2	count	types-after
+	 * ...
+	 *
+	 * @param bloomFilterSize the size of the Bloom filter, in bits
+	 * @param filename path to the statistics file
+	 */
 	private void populateBloomFilter(int bloomFilterSize, String filename) {
 		HashMap<String,Long> typesAfter = new HashMap<String,Long>();
 		try {
@@ -349,6 +459,17 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return;
 	}
 
+	/**
+	 * Estimate the number of objects that will be stored in the Bloom
+	 * filter. The optimum number of hash functions depends on the number
+	 * of items that will be stored, so we want a guess before we begin to
+	 * read the statistics file and store it.
+	 *
+	 * @param source an InputStream pointing to the training corpus stats
+	 *
+	 * @return an estimate of the number of objects to be stored in the
+	 * Bloom filter
+	 */
 	private int estimateNumberOfObjects(InputStream source)
 	{
 		Scanner scanner = new Scanner(source);
@@ -376,6 +497,16 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return (int) Math.round(numLines * estimate);
 	}
 	
+	/**
+	 * Reads the statistics from a source and stores them in the Bloom
+	 * filter. The ngram counts are stored immediately in the Bloom filter,
+	 * but the counts of distinct types following each ngram are accumulated
+	 * from the file as we go.
+	 *
+	 * @param source an InputStream pointing to the statistics
+	 * @param types a HashMap that will stores the accumulated counts of
+	 * distinct types observed to follow each ngram
+	 */
 	private void populateFromInputStream(InputStream source, HashMap<String,Long> types) {
 		Scanner scanner = new Scanner(source);
 		numTokens = Double.NEGATIVE_INFINITY; // = log(0)
@@ -409,9 +540,14 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		return;
 	}
 	
-	/*
-	 * this corresponds to Talbot and Osborne's "Tera-scale LMs on the
+	/**
+	 * Adds an ngram, along with an associated value, to the Bloom filter.
+	 * This corresponds to Talbot and Osborne's "Tera-scale LMs on the
 	 * cheap", algorithm 1.
+	 *
+	 * @param ngram an array representing the ngram
+	 * @param value the value to be associated with the ngram
+	 * @param funcs an array of long to be used as hash functions
 	 */
 	private void add(int [] ngram, long value, long [][] funcs) {
 		if (ngram == null) return;
@@ -422,13 +558,17 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		}
 	}
 	
+	/**
+	 * Read a Bloom filter LM from an external file.
+	 *
+	 * @param in an ObjectInput stream to read from
+	 */
 	public void readExternal(ObjectInput in)
 	throws IOException, ClassNotFoundException {
 		vocabulary = new Vocabulary();
 		int vocabSize = in.readInt();
 		for (int i = 0; i < vocabSize; i++) {
 			String line = in.readUTF();
-		//	System.err.println(line);
 			vocabulary.addTerminal(line);
 		}
 		numTokens = in.readDouble();
@@ -447,6 +587,13 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		bf.readExternal(in);
 	}
 	
+	/**
+	 * Write a Bloom filter LM to some external location.
+	 *
+	 * @param out an ObjectOutput stream to write to
+	 *
+	 * @throws IOException if an input or output exception occurred
+	 */
 	public void writeExternal(ObjectOutput out) throws IOException {
 		out.writeInt(vocabulary.size());
 		for (int i = 0; i < vocabulary.size(); i++) {
@@ -474,20 +621,15 @@ public class BloomFilterLanguageModel extends AbstractLM implements Externalizab
 		throw new UnsupportedOperationException("probabilityOfBackoffState_helper undefined for bloom filter LM");
 	}
 	
-	/*
-	
-	 * this is an abstract method from DefaultNGramLanguageModel
-	 * it returns the probability of an ngram.
-	 
-	public double ngramLogProbability(int [] ngram, int order) {
-		int [] lm_ngram = new int[ngram.length];
-		for (int i = 0; i < ngram.length; i++) {
-			lm_ngram[i] = TMtoLMMapping[ngram[i]];
-		}
-		return wittenBell(lm_ngram, order);
-	}*/
-	
-
+	/**
+	 * Returns the language model score for an n-gram. This is called from
+	 * the rest of the Joshua decoder.
+	 *
+	 * @param ngram the ngram to score
+	 * @param order the order of the model
+	 *
+	 * @return the language model score of the ngram
+	 */
 	@Override
 	protected double ngramLogProbability_helper(int[] ngram, int order) {
 		int [] lm_ngram = new int[ngram.length];
