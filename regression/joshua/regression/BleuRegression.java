2de943bed7a820721a1d4c6442b7bc535fbb63ef
dowobeha <dowobeha@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2009-12-03 19:30:56 +0000

    Fixed messed up package name
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@1218 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- regression/joshua/regression/BleuRegression.java
+++ regression/joshua/regression/BleuRegression.java
@@ -15,7 +15,7 @@
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
  */
-package regression.joshua.regression;
+package joshua.regression;
 
 import java.io.File;
 import java.io.FileNotFoundException;
