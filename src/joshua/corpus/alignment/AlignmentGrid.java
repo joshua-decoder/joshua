d57bd6431948c6131c5368ee827fa07dd702ebb9
callison-burch <callison-burch@0ae5e6b2-d358-4f09-a895-f82f13dd62a4>
2010-01-07 22:31:21 +0000

    Experimenting with larger grid sizes
    git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@1288 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

--- src/joshua/corpus/alignment/AlignmentGrid.java
+++ src/joshua/corpus/alignment/AlignmentGrid.java
@@ -50,12 +50,12 @@ public class AlignmentGrid implements Externalizable {
 	/**
 	 * Maximum size of a dimension.
 	 */
-	static public final int MAX_LENGTH = 100;
+	static public final int MAX_LENGTH = 1000;
 
 	/**
 	 * Constant used for generating coordinate short value.
 	 */
-	static private final short X_SHIFT = 100;
+	static private final short X_SHIFT = 1000;
 
 	//===============================================================
 	// Member variables
