joshua-decoder/joshua

commit 86d39338dc2b36a24d9fd56ed0f46c0fbb0ea06a

Experimenting with better handling of long sentences
git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@1291 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

src/joshua/corpus/alignment/AlignmentGrids.java
@@ -152,13 +152,21 @@ public class AlignmentGrids extends AbstractAlignmentGrids {
 		// Write the widths of each grid
 		logger.fine("Exporting widths: " + size + " integers (" + size*4 + ") bytes");
 		for (AlignmentGrid grid : alignments) {
+		    if(grid != null) {
 			out.writeInt(grid.width);
+		    } else {
+			out.writeInt(0);
+		    }
 		}
 		
 		// Write the widths of each grid
 		logger.fine("Exporting widths: " + size + " integers (" + size*4 + ") bytes");
 		for (AlignmentGrid grid : alignments) {
+		    if(grid != null) {
 			out.writeInt(grid.height);
+		    } else {
+			out.writeInt(0);
+		    }
 		}
 		
 		// Write the number of alignment points in each grid
@@ -166,8 +174,12 @@ public class AlignmentGrids extends AbstractAlignmentGrids {
 		int pointCounter = 0;
 		out.writeInt(pointCounter);
 		for (AlignmentGrid grid : alignments) {
+		    if(grid != null) {
 			pointCounter += grid.coordinates.length; 
 			out.writeInt(pointCounter);
+		    } else {
+			out.writeInt(0);
+		    }
 		}
 		logger.finer("\tfinal pointCounter value was: " + pointCounter);
 
@@ -175,17 +187,21 @@ public class AlignmentGrids extends AbstractAlignmentGrids {
 		// Write the alignment points
 		logger.fine("Exporting grid coordinates: " + pointCounter + " shorts (" + pointCounter*2 + ") bytes");
 		for (AlignmentGrid grid : alignments) {
+		    if(grid != null) {
 			for (short point : grid.coordinates) {
 				out.writeShort(point);
 			}
+		    }
 		}
 		
 		// Write the reverse alignment points
 		logger.fine("Exporting reverse grid coordinates: " + pointCounter + " shorts (" + pointCounter*2 + ") bytes");
 		for (AlignmentGrid grid : alignments) {
+		    if(grid != null) {
 			for (short point : grid.transposedCoordinates) {
 				out.writeShort(point);
 			}
+		    }
 		}
 		
 	}

commit 4947c33fb41290bc72ff42d73006741e34837b3a

Fixed bug introduced in r1291.
The changes in r1291 were intended to better handle (by properly ignoring) sentences which were too long for grammar extraction to handle. This is fine, except that the pointCounter value for ignored long sentences was incorrectly writeExternal'ed as zero in r1291. The pointCounter variable is defined as a strictly non-decreasing counter value. Thus, the proper way to ignore a sentence, as far as pointCounter is concerned, is to increment by zero (that is, do nothing) rather than resetting the value to zero.

Unfortunately, it appears that proper unit tests have not been written to check for this case. This needs to be done.

git-svn-id: https://joshua.svn.sf.net/svnroot/joshua/trunk@1710 0ae5e6b2-d358-4f09-a895-f82f13dd62a4

src/joshua/corpus/alignment/AlignmentGrids.java
@@ -159,8 +159,8 @@ public class AlignmentGrids extends AbstractAlignmentGrids {
 		    }
 		}
 		
-		// Write the widths of each grid
-		logger.fine("Exporting widths: " + size + " integers (" + size*4 + ") bytes");
+		// Write the heights of each grid
+		logger.fine("Exporting heights: " + size + " integers (" + size*4 + ") bytes");
 		for (AlignmentGrid grid : alignments) {
 		    if(grid != null) {
 			out.writeInt(grid.height);
@@ -178,7 +178,8 @@ public class AlignmentGrids extends AbstractAlignmentGrids {
 			pointCounter += grid.coordinates.length; 
 			out.writeInt(pointCounter);
 		    } else {
-			out.writeInt(0);
+		    	out.writeInt(pointCounter);
+			//out.writeInt(0);
 		    }
 		}
 		logger.finer("\tfinal pointCounter value was: " + pointCounter);