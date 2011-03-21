cf8fae2948d81aa2f8a7190157d1380eaeab6f00
Juri Ganitkevitch <j.ganitkevitch@gmail.com>
2011-03-20 23:12:00 -0400

    Improvements to paraphrase grammar extraction.

--- src/joshua/tools/BuildParaphraseGrammar.java
+++ src/joshua/tools/BuildParaphraseGrammar.java
@@ -35,13 +35,19 @@ import joshua.util.io.LineReader;
 public class BuildParaphraseGrammar {
 	
 	/** Logger for this class. */
-	private static final Logger	logger				= Logger.getLogger(BuildParaphraseGrammar.class.getName());
+	private static final Logger	logger			= Logger.getLogger(BuildParaphraseGrammar.class.getName());
 	
 	private static boolean			HIERO_MODE	= false;
 	
-	private static int          MIN_COUNT     = 3;
-	private static double       MIN_PROB      = -Math.log(0.0001);
-	private static int          TOP_K         = 25;
+	private static double 			MIN_COUNT		= -Math.log(3.0);
+	private static double				MIN_PROB		= -Math.log(0.0001);
+	private static int					TOP_K				= 25;
+	
+	private static long					PRUNED_COUNT	= 0;
+	private static long         PRUNED_PROB		= 0;
+	private static long					PRUNED_TOP_K	= 0;
+	
+	private static long					GENERATED    	= 0;
 	
 	/**
 	 * Main method.
@@ -76,7 +82,7 @@ public class BuildParaphraseGrammar {
 			else if ("-min_p".equals(args[i]))
 				MIN_PROB = -Math.log(Double.parseDouble(args[++i]));
 			else if ("-min_c".equals(args[i]))
-				MIN_COUNT = Integer.parseInt(args[++i]);
+				MIN_COUNT = -Math.log(Double.parseDouble(args[++i]));
 		}
 		if (grammar_file_name == null) {
 			logger.severe("a grammar file is required for operation");
@@ -89,14 +95,26 @@ public class BuildParaphraseGrammar {
 		while (grammarReader.ready()) {
 			String line = grammarReader.readLine();
 			
-			String[] fields = line.split("#");
+			String delimiter = (HIERO_MODE ? " \\|\\|\\| " : "#");
+			
+			String[] fields = line.split(delimiter);
 			if (fields[0].equals("@_COUNT") || fields.length != 4)
 				continue;
 			
-			String src = fields[0];
-			String tgt = fields[1];
-			String head = fields[2];
-			String feature_values = fields[3];
+			String src, tgt, head, feature_values;
+			
+			if (HIERO_MODE) {
+				src = fields[1];
+				tgt = fields[2];
+				head = fields[0];
+				feature_values = fields[3];
+			}
+			else {
+				src = fields[0];
+				tgt = fields[1];
+				head = adaptNonterminalMarkup(fields[2]);
+				feature_values = fields[3];
+			}
 			
 			if (current_batch.fits(src, tgt, head, feature_values))
 				current_batch.addRule(tgt, feature_values);
@@ -106,15 +124,22 @@ public class BuildParaphraseGrammar {
 			}
 		}
 		current_batch.process();
+		
+		System.err.println("Pivoting completed. Pruning statistics: \n" +
+				"\t" + PRUNED_PROB + " via probability threshold (" + Math.exp(-MIN_PROB) + ").\n" +
+				"\t" + PRUNED_TOP_K + " via top " + TOP_K + ".\n" +
+				"\t" + PRUNED_COUNT + " via count threshold (" + Math.exp(-MIN_COUNT) + ").\n" +
+				"Total rules generated: " + GENERATED);
 	}
 	
 	static class RuleBatch {
 		
-		String											src		= null;
-		String											head	= null;
-		String[]										NTs		= { null, null };
+		String								src			= null;
+		String								head		= null;
+		String[]							NTs			= { null, null };
+		int										ntCount	= 0;
 		
-		List<TranslationRule>				translationRules;
+		List<TranslationRule>	translationRules;
 		
 		
 		public RuleBatch() {
@@ -136,10 +161,12 @@ public class BuildParaphraseGrammar {
 
 		private void extractNTs() {
 			String[] src_tokens = src.split("\\s");
-			int nt_count = 0;
+			ntCount = 0;
 			for (String src_token : src_tokens)
-				if (src_token.startsWith("@"))
-					NTs[nt_count++] = src_token;
+				if (!HIERO_MODE && src_token.startsWith("@"))
+					NTs[ntCount++] = adaptNonterminalMarkup(src_token, ntCount);
+				else if (HIERO_MODE && src_token.startsWith("[X,"))
+					NTs[ntCount++] = src_token;
 		}
 		
 
@@ -155,32 +182,51 @@ public class BuildParaphraseGrammar {
 		
 
 		public void addRule(String tgt, String feature_values) {
-			TranslationRule candidate = new TranslationRule(tgt, feature_values, NTs);
+			TranslationRule candidate = new TranslationRule(tgt, feature_values, NTs, ntCount);
 			
-			// pre-pruning - at least a count of ten and a translation prob. of 0.001
-			if ((Math.exp(-candidate.feature_vector[6]) > MIN_COUNT || HIERO_MODE) && candidate.feature_vector[4] < MIN_PROB)
-				translationRules.add(candidate);
+			// pre-pruning - minimum count (for SAMT) and probability threshold
+			if (!HIERO_MODE) {
+				if (candidate.feature_vector[6] > MIN_COUNT)
+					PRUNED_COUNT++;
+				else if (candidate.feature_vector[4] > MIN_PROB)
+					PRUNED_PROB++;
+				else					
+					translationRules.add(candidate);
+			} else {
+				if (candidate.feature_vector[9] > MIN_PROB)
+					PRUNED_PROB++;
+				else
+					translationRules.add(candidate);
+			}
 		}
 		
 
 		public void process() {
 			List<ParaphraseRule> paraphraseRules = new ArrayList<ParaphraseRule>();
-
+			
 			Comparator<ParaphraseRule> c = new Comparator<ParaphraseRule>() {
 				public int compare(ParaphraseRule a, ParaphraseRule b) {
-					double a_value = a.feature_values[3] + a.feature_values[4] + a.feature_values[13] + a.feature_values[14];
-					double b_value = b.feature_values[3] + b.feature_values[4] + b.feature_values[13] + b.feature_values[14];
-					return (a_value - b_value >= 0) ? 1 : -1;
+					double a_value = a.feature_values[3] + a.feature_values[4] + a.feature_values[5] + a.feature_values[6];
+					double b_value = b.feature_values[3] + b.feature_values[4] + b.feature_values[5] + b.feature_values[6];
+					return (a_value - b_value <= 0) ? -1 : 1;
 				}
 			};
 			
 			for (TranslationRule from : translationRules) {
-				for (TranslationRule to : translationRules)
-					paraphraseRules.add(from.pivotToParaphraseRule(to, head));
-				
+				for (TranslationRule to : translationRules) {
+					ParaphraseRule new_rule = (HIERO_MODE ? ParaphraseRule.pivotHieroStyle(from, to, head) : ParaphraseRule.pivotSamtStyle(from, to, head)); 
+					if (new_rule != null)
+						paraphraseRules.add(new_rule);
+				}
+					
 				Collections.sort(paraphraseRules, c);
-				for (int k = 0; k < Math.min(TOP_K, paraphraseRules.size()); k++)
-					System.out.println(paraphraseRules.get(k));
+				for (int k = 0; k < Math.min(TOP_K, paraphraseRules.size()); k++) {
+					ParaphraseRule a = paraphraseRules.get(k);
+					System.out.println(a.feature_values[3] + a.feature_values[4] + a.feature_values[5] + a.feature_values[6] + "\t" + paraphraseRules.get(k));
+				}
+				GENERATED += Math.min(TOP_K, paraphraseRules.size());
+				if (paraphraseRules.size() > TOP_K) 
+					PRUNED_TOP_K += paraphraseRules.size() - TOP_K;
 				paraphraseRules.clear();
 			}
 		}
@@ -191,6 +237,7 @@ public class BuildParaphraseGrammar {
 		String[]	tgt_tokens;
 		String[]	NTs;
 		double[]	feature_vector;
+		int				arity;
 		
 		String		sourceSide;
 		
@@ -201,9 +248,12 @@ public class BuildParaphraseGrammar {
 		boolean		adjacent_nts			= false;
 		boolean		no_lexical_tokens	= false;
 		
+		double    avg_word_length   = 0.0;
 		
-		public TranslationRule(String tgt, String feature_values, String[] NTs) {
+		
+		public TranslationRule(String tgt, String feature_values, String[] NTs, int nt_count) {
 			this.NTs = NTs;
+			this.arity = nt_count;
 			
 			tgt_tokens = tgt.split("\\s");
 			
@@ -214,99 +264,38 @@ public class BuildParaphraseGrammar {
 			
 			StringBuffer source_side_buffer = new StringBuffer(tgt.length() + 10);
 			for (int j = 0; j < tgt_tokens.length; j++) {
-				if (tgt_tokens[j].equals("@1")) {
-					first_nt_pos = j;
-					source_side_buffer.append(NTs[0]);
-				} else if (tgt_tokens[j].equals("@2")) {
-					second_nt_pos = j;
-					source_side_buffer.append(NTs[1]);
-				} else
-					source_side_buffer.append(tgt_tokens[j]);
+				if (HIERO_MODE) {
+					if (tgt_tokens[j].equals("[X,1]")) {
+						first_nt_pos = j;
+						source_side_buffer.append(NTs[0]);
+					} else if (tgt_tokens[j].equals("[X,2]")) {
+						second_nt_pos = j;
+						source_side_buffer.append(NTs[1]);
+					} else {
+						source_side_buffer.append(tgt_tokens[j]);
+						avg_word_length += tgt_tokens[j].length();
+					}
+				} else {
+					if (tgt_tokens[j].equals("@1")) {
+						first_nt_pos = j;
+						source_side_buffer.append(NTs[0]);
+					} else if (tgt_tokens[j].equals("@2")) {
+						second_nt_pos = j;
+						source_side_buffer.append(NTs[1]);
+					} else {
+						source_side_buffer.append(tgt_tokens[j]);
+						avg_word_length += tgt_tokens[j].length();
+					}
+				}
 				source_side_buffer.append(" ");
 			}
 			source_side_buffer.deleteCharAt(source_side_buffer.length() - 1);
 			sourceSide = source_side_buffer.toString();
 			
-			no_lexical_tokens = (tgt_tokens.length == NTs.length);
-			adjacent_nts = (first_nt_pos >= 0) && (second_nt_pos >= 0) && (Math.abs(first_nt_pos - second_nt_pos) == 1);
-			non_monotonic = (first_nt_pos >= 0) && (second_nt_pos >= 0) && (first_nt_pos > second_nt_pos);
-		}
-		
-
-		protected ParaphraseRule pivotToParaphraseRule(TranslationRule map_to, String rule_head) {
-		
-			// merge feature vectors
-			double[] src = this.feature_vector;
-			double[] tgt = map_to.feature_vector;
-			
-			double[] merged = new double[18];
-			
-			// TODO: more graceful and flexible handling of this
-			if (src.length != 23) {
-				logger.severe("number of features doesn't match up: expecting 23, seeing " + src.length);
-				System.exit(1);
-			}
-			
-			// glue rule feature - we don't produce glue grammars
-			merged[0] = 0;
-			// rule application counter
-			merged[1] = 1;
-			// target word counter
-			merged[2] = tgt[2];
-			
-			// -log p(e2 | e1)
-			merged[3] = src[3] + tgt[5];
-			// -log p(e1 | e2)			
-			merged[4] = src[5] + tgt[3];
-			
-			// purely-lexical feature (rule body consists only of terminals)
-			merged[5] = tgt[8];
-			// adjacent-NT feature
-			merged[6] = adjacent_nts ? 1 : 0;
-			// _X rule feature
-			merged[7] = tgt[10];
-			
-			// source terminals but no target terminals feature
-			merged[8] = (!this.no_lexical_tokens && map_to.no_lexical_tokens) ? 1 : 0;
-			
-			// source-punctuation-was-removed feature
-			// TODO: lacks some precision in case of multi-punctuation
-			merged[9] = (src[14] == 0 && tgt[14] == 1) ? 1 : 0;
-			
-			// non-monotonicity penalty - verified
-			merged[10] = (this.non_monotonic == map_to.non_monotonic) ? 0 : 1;
-			
-			// rareness penalty (we use the one for the rarer rule)
-			merged[11] = Math.max(src[16], tgt[16]);
-						
-			// null rule feature
-			merged[12] = (src[18] + tgt[18] >= 1) ? 1 : 0;
-			
-			// various IBM1 scores
-			merged[13] = src[19] + tgt[21];
-			merged[14] = src[21] + tgt[19];
-			merged[15] = src[20] + tgt[22];
-			merged[16] = src[22] + tgt[20];
-			
-			if (this.sourceSide.equals(map_to.sourceSide))
-				merged[merged.length - 1] = 1;
-			else
-				merged[merged.length - 1] = 0;
-			
-			// build rule target side
-			StringBuffer tgt_buffer = new StringBuffer();
-			for (int i = 0; i < map_to.tgt_tokens.length; i++) {
-				if (i == map_to.first_nt_pos)
-					tgt_buffer.append(!this.non_monotonic ? "@1" : "@2");
-				else if (i == map_to.second_nt_pos)
-					tgt_buffer.append(!this.non_monotonic ? "@2" : "@1");
-				else
-					tgt_buffer.append(map_to.tgt_tokens[i]);
-				tgt_buffer.append(" ");
-			}
-			tgt_buffer.deleteCharAt(tgt_buffer.length() - 1);
-			
-			return new ParaphraseRule(sourceSide, tgt_buffer.toString(), rule_head, merged);
+			no_lexical_tokens = (tgt_tokens.length == arity);
+			adjacent_nts = (arity == 2) && (Math.abs(first_nt_pos - second_nt_pos) == 1);
+			non_monotonic = (arity == 2) && (first_nt_pos > second_nt_pos);
+			avg_word_length = (no_lexical_tokens ? 0 : avg_word_length / (tgt_tokens.length - arity));
 		}
 	}
 	
@@ -327,20 +316,187 @@ public class BuildParaphraseGrammar {
 		
 
 		public String toString() {
-			// build rule output
 			StringBuffer rule_buffer = new StringBuffer();
-			rule_buffer.append(src);
-			rule_buffer.append("#");
-			rule_buffer.append(tgt);
-			rule_buffer.append("#");
 			rule_buffer.append(head);
-			rule_buffer.append("#");
+			rule_buffer.append(" ||| ");
+			rule_buffer.append(src);
+			rule_buffer.append(" ||| ");
+			rule_buffer.append(tgt);
+			rule_buffer.append(" ||| ");
+			
 			for (double value : feature_values) {
 				rule_buffer.append(value);
 				rule_buffer.append(" ");
 			}
 			rule_buffer.deleteCharAt(rule_buffer.length() - 1);
+
 			return rule_buffer.toString();
 		}
+		
+
+		public static ParaphraseRule pivotSamtStyle(TranslationRule from, TranslationRule to, String rule_head) {
+			
+			// merge feature vectors
+			double[] src = from.feature_vector;
+			double[] tgt = to.feature_vector;
+			
+			double[] merged = new double[16];
+			
+			// TODO: more graceful and flexible handling of this
+			if (src.length != 23) {
+				logger.severe("number of features doesn't match up: expecting 23, seeing " + src.length);
+				System.exit(1);
+			}
+			
+			// glue rule feature - we don't produce glue grammars
+			merged[0] = 0;
+			// rule application counter
+			merged[1] = 1;
+			
+			if (from.sourceSide.equals(to.sourceSide))
+				merged[2] = 1;
+			else
+				merged[2] = 0;
+			
+			// -log p(e2 | e1)
+			merged[3] = src[3] + tgt[5];
+			// -log p(e1 | e2)
+			merged[4] = src[5] + tgt[3];
+			
+			// -log lex(e2 | e1)
+			merged[5] = src[19] + tgt[21];
+			// -log lex(e1 | e2)
+			merged[6] = src[21] + tgt[19];
+			
+			// source word counter
+			merged[7] = from.tgt_tokens.length - from.arity;
+			// target word counter
+			merged[8] = to.tgt_tokens.length - to.arity;
+			
+			// word count difference
+			merged[9] = merged[8] - merged[7];
+			
+			// purely-lexical feature (rule body consists only of terminals)
+			merged[10] = (to.arity == 0 ? 1 : 0);
+
+			// source terminals but no target terminals indicator
+			merged[11] = (!from.no_lexical_tokens && to.no_lexical_tokens) ? 1 : 0;
+			
+			// non-monotonicity penalty
+			merged[12] = (from.non_monotonic == to.non_monotonic) ? 0 : 1;
+			
+			merged[13] = to.avg_word_length - from.avg_word_length ;
+			
+			// rareness penalty (we use the one for the rarer rule)
+			merged[14] = Math.max(src[17], tgt[17]);
+			
+			// adjacent-NT feature
+			merged[15] = from.adjacent_nts ? 1 : 0;
+			
+			
+			// build rule target side
+			StringBuffer tgt_buffer = new StringBuffer();
+			for (int i = 0; i < to.tgt_tokens.length; i++) {
+				if (i == to.first_nt_pos)
+					tgt_buffer.append(!from.non_monotonic ? from.NTs[0] : from.NTs[1]);
+				else if (i == to.second_nt_pos)
+					tgt_buffer.append(!from.non_monotonic ? from.NTs[1] : from.NTs[0]);
+				else
+					tgt_buffer.append(to.tgt_tokens[i]);
+				tgt_buffer.append(" ");
+			}
+			tgt_buffer.deleteCharAt(tgt_buffer.length() - 1);
+			
+			return new ParaphraseRule(from.sourceSide, tgt_buffer.toString(), rule_head, merged);
+		}
+		
+
+		public static ParaphraseRule pivotHieroStyle(TranslationRule from, TranslationRule to, String rule_head) {
+			
+			// merge feature vectors
+			double[] src = from.feature_vector;
+			double[] tgt = to.feature_vector;
+			
+			double[] merged = new double[14];
+			
+			// TODO: more graceful and flexible handling of this
+			if (src.length != 13) {
+				logger.severe("number of features doesn't match up: expecting 13, seeing " + src.length);
+				System.exit(1);
+			}
+			
+			if (from.no_lexical_tokens || from.adjacent_nts) {
+				System.err.println("ARR");
+				return null;
+			}
+				
+			// glue rule feature - we don't produce glue grammars
+			merged[0] = 0;
+			// rule application counter
+			merged[1] = 1;
+			
+			if (from.sourceSide.equals(to.sourceSide))
+				merged[2] = 1;
+			else
+				merged[2] = 0;
+			
+			// -log p(e2 | e1)
+			merged[3] = src[9] + tgt[11];
+			// -log p(e1 | e2)
+			merged[4] = src[11] + tgt[9];
+			
+			// -log lex(e2 | e1)
+			merged[5] = src[5] + tgt[6];
+			// -log lex(e1 | e2)
+			merged[6] = src[6] + tgt[5];
+			
+			// source word counter
+			merged[7] = from.tgt_tokens.length - from.arity;
+			// target word counter
+			merged[8] = to.tgt_tokens.length - to.arity;
+			
+			// word count difference
+			merged[9] = merged[8] - merged[7];
+			
+			// purely-lexical feature (rule body consists only of terminals)
+			merged[10] = tgt[4];
+			
+			// source terminals but no target terminals indicator
+			merged[11] = (!from.no_lexical_tokens && to.no_lexical_tokens) ? 1 : 0;
+			
+			// non-monotonicity penalty
+			merged[12] = (from.non_monotonic == to.non_monotonic) ? 0 : 1;
+			
+			merged[13] = to.avg_word_length - from.avg_word_length;
+			
+			// build rule target side
+			StringBuffer tgt_buffer = new StringBuffer();
+			for (int i = 0; i < to.tgt_tokens.length; i++) {
+				if (i == to.first_nt_pos)
+					tgt_buffer.append(!from.non_monotonic ? "[X,1]" : "[X,2]");
+				else if (i == to.second_nt_pos)
+					tgt_buffer.append(!from.non_monotonic ? "[X,2]" : "[X,1]");
+				else
+					tgt_buffer.append(to.tgt_tokens[i]);
+				tgt_buffer.append(" ");
+			}
+			tgt_buffer.deleteCharAt(tgt_buffer.length() - 1);
+			
+			return new ParaphraseRule(from.sourceSide, tgt_buffer.toString(), rule_head, merged);
+		}
+	}
+	
+	protected static String adaptNonterminalMarkup(String nt, int index) {
+		// changes SAMT markup to Hiero-style
+		return "[" + nt.replaceAll(",", "_COMMA_")
+			.replaceAll("\\$", "_DOLLAR_")
+			.replaceAll("@", "") + "," + index + "]";
+	}
+	
+	protected static String adaptNonterminalMarkup(String nt) {
+		// changes SAMT markup to Hiero-style
+		return "[" + nt.replaceAll(",", "_COMMA_")
+			.replaceAll("\\$", "_DOLLAR_")
+			.replaceAll("@", "") + "]";
 	}
 }
