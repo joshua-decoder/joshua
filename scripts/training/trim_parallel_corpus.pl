5195a36631e92c690d047893e69fa1d041404fe9
Matt Post <post@jhu.edu>
2011-06-27 15:50:11 -0400

    modified to take tab-delimited input (which it splits) instead of two file names to open

--- scripts/training/trim_parallel_corpus.pl
+++ scripts/training/trim_parallel_corpus.pl
@@ -6,18 +6,12 @@
 # threshold.  file1 is output to STDOUT, file2 to STDERR.
 
 # e.g.,
-# trim_parallel_corpus.pl corpus.en corpus.fr 40 > corpus.en.40 2> corpus.fr.40
+# paste corpus.en.corpus.fr | trim_parallel_corpus.pl 40 > corpus.en.40 2> corpus.fr.40
 
-my $lang1_file = shift;
-my $lang2_file = shift;
 my $thresh = shift || 100;
 
-open READ1, $lang1_file or die;
-open READ2, $lang2_file or die;
-
-for (;;) {
-  my $line1 = <READ1>;
-  my $line2 = <READ2>;
+while (<>) {
+  my ($line1,$line2) = split(/\t/,$_,2);
 
   last unless $line1 and $line2;
 
