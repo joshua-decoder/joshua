e0df5d2373d49e3d1920fd2f3f2d5ba28f949c91
Juri Ganitkevitch <j.ganitkevitch@gmail.com>
2012-02-23 16:46:43 -0500

    Meddled with SAMT test & packed grammars.

--- test/bn-en/samt/test_packed.sh
+++ test/bn-en/samt/test_packed.sh
@@ -2,11 +2,11 @@
 
 set -u
 
-./decoder_packed 2> log
+./decode_packed 2> log_packed
 
-java -cp $JOSHUA/bin -Dfile.encoding=utf8 -Djava.library.path=lib -Xmx256m -Xms256m -Djava.util.logging.config.file=logging.properties joshua.util.JoshuaEval -cand output -format nbest -ref reference.en -rps 4 -m BLEU 4 closest > output.bleu
+java -cp $JOSHUA/bin -Dfile.encoding=utf8 -Djava.library.path=lib -Xmx256m -Xms256m -Djava.util.logging.config.file=logging.properties joshua.util.JoshuaEval -cand output_packed -format nbest -ref reference.en -rps 4 -m BLEU 4 closest > output_packed.bleu
 
-diff -u output.bleu output.gold.bleu > diff
+diff -u output_packed.bleu output.gold.bleu > diff
 
 if [ $? -eq 0 ]; then
 	echo PASSED
