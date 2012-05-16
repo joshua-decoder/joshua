209ea4dced56ba5142ec68d63d158ff72939763c
Matt Post <post@cs.jhu.edu>
2012-05-16 14:05:11 -0400

    added make-release script and build target

---
+++ scripts/support/make-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script packages up an end-user version of Joshua for download.
+
+set -u
+
+version=$1
+
+cd $JOSHUA
+ant clean java documentation
+[[ ! -d release ]] && mkdir release
+ln -s $JOSHUA joshua-$version
+tar czf release/joshua-$version.tgz \
+    --exclude '*~ #*' \
+    joshua-$version/{README.txt,INSTALL.txt,build.xml,logging.properties} \
+    joshua-$version/bin \
+    joshua-$version/lib/{*jar,eng_sm6.gr,README,LICENSES} \
+    joshua-$version/doc \
+    joshua-$version/scripts \
+    joshua-$version/examples \
+    joshua-$version/tree_visualizer
+
+rm -f joshua-version
