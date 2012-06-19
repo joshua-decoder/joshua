317e2dae34a996237214f1abcc6541e2af3a8fd6
Matt Post <post@cs.jhu.edu>
2012-06-18 22:11:01 -0400

    renamed test file so as not to find self in pattern

---
+++ test/run-all-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Runs all the test cases (scripts named test*.sh) beneath this directory, reporting success or
+# failure.  Each test script should echo "PASSED" (and return 0) on success or "FAILED" (and return
+# nonzero) on failure.
+#
+# Important!  Do not rename this script to match the pattern test*.sh, or it will execute
+# recursively.
+
+for file in $(find . -name test*.sh); do
+  if [[ ! -x $file ]]; then
+    continue;
+  fi
+
+	dir=$(dirname $file)
+
+	echo -n "Running test in $dir..."
+	pushd $dir > /dev/null
+	bash test.sh
+
+	# fail on failure
+	if [[ $? -ne 0 ]]; then
+		exit 1
+	fi
+
+	popd > /dev/null
+done
