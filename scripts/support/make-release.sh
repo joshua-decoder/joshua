1e985575c3443515c284348fb5bab8dd4550ef59
Matt Post <post@cs.jhu.edu>
2012-06-29 13:55:15 -0400

    added joshua-decoder script, renamed README, added user documentation

--- scripts/support/make-release.sh
+++ scripts/support/make-release.sh
@@ -11,29 +11,18 @@ ant clean java documentation
 [[ ! -d release ]] && mkdir release
 ln -s $JOSHUA joshua-$version
 
-# version without docs
+wget -r joshua-decoder.org
+
 tar czf release/joshua-$version.tgz \
     --exclude='*~' --exclude='#*' \
-    joshua-$version/{README.txt,build.xml,logging.properties} \
+    joshua-$version/{README,build.xml,logging.properties} \
     joshua-$version/src \
     joshua-$version/bin \
     joshua-$version/lib/{*jar,eng_sm6.gr,README,LICENSES} \
     joshua-$version/scripts \
     joshua-$version/examples \
     joshua-$version/thrax/bin/thrax.jar \
-    joshua-$version/tree_visualizer
-
-# docs version
-tar czf release/joshua-$version-with-docs.tgz \
-    --exclude='*~' --exclude='#*' \
-    joshua-$version/{README.txt,build.xml,logging.properties} \
-    joshua-$version/src \
-    joshua-$version/bin \
-    joshua-$version/lib/{*jar,eng_sm6.gr,README,LICENSES} \
-    joshua-$version/doc \
-    joshua-$version/scripts \
-    joshua-$version/examples \
-    joshua-$version/thrax/bin/thrax.jar \
-    joshua-$version/tree_visualizer
+		joshua-$version/joshua-decoder.org \
+    joshua-$version/{joshua-decoder,extract-1best,visualize.sh} 
 
 rm -f joshua-$version
