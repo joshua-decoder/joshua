983af2db1af8dc9608424b98a5e39aa7f936d9e9
Matt Post <post@cs.jhu.edu>
2012-06-29 22:18:01 -0400

    updated README and examples

--- examples/example/decode_example_javalm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java  -classpath $CLASSPATH:./bin \
-      -Xmx1000m -Xms1000m         \
-      joshua.decoder.JoshuaDecoder example/example.config.javalm example/example.test.in example/example.nbest.javalm.out
