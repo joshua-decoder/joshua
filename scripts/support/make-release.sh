ff74daab50d0ec0763a02cb08f78c2fcfc6a2dd9
Matt Post <post@cs.jhu.edu>
2012-07-24 12:41:16 -0400

    added data/ directory to tarball

--- scripts/support/make-release.sh
+++ scripts/support/make-release.sh
@@ -20,6 +20,7 @@ tar czf release/joshua-$version.tgz \
     joshua-$version/bin \
     joshua-$version/lib/{*jar,eng_sm6.gr,hadoop-0.20.2.tar.gz,README,LICENSES} \
     joshua-$version/scripts \
+    joshua-$version/data \
     joshua-$version/examples \
     joshua-$version/thrax/bin/thrax.jar \
 		joshua-$version/joshua-decoder.org \
