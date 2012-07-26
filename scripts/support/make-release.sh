f633fb3c14e977a4a77343d78b2c2c136902a95c
Matt Post <post@cs.jhu.edu>
2012-07-26 09:15:01 -0400

    added test/ directory to the release tarball

--- scripts/support/make-release.sh
+++ scripts/support/make-release.sh
@@ -21,6 +21,7 @@ tar czf release/joshua-$version.tgz \
     joshua-$version/lib/{*jar,eng_sm6.gr,hadoop-0.20.2.tar.gz,README,LICENSES} \
     joshua-$version/scripts \
     joshua-$version/data \
+    joshua-$version/test \
     joshua-$version/examples \
     joshua-$version/thrax/bin/thrax.jar \
 		joshua-$version/joshua-decoder.org \
