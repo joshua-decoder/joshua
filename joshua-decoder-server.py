00b63a1bf1daf6da9daf1484a383db4ca1f5b0a8
Luke Orland <orluke@gmail.com>
2012-08-22 10:55:34 -0400

    keep the joshua-decoder subprocess alive for decoding more inputs from the client.

--- joshua-decoder-server.py
+++ joshua-decoder-server.py
@@ -3,27 +3,69 @@
 """
 This sever is based on the code at
 http://kmkeen.com/socketserver/
+
+USAGE:
+    1) Start the server with the command:
+        ./joshua-decoder-server.py [optional joshua-decoder arguments]
+    2) Use a client to send source segments and receive decoded outputs
+    (translations). E.g.:
+        echo hola | nc localhost 2000
+
+TODO:
+* Figure out a way around starting the joshua-decoder subprocess as a global
+  object.
+* command-line options
+  + first argument is required positional port number
+  + rest of arguments are optional, passed directly to joshua-decoder
+* multithreading
 """
 
 import os
 import SocketServer
 import sys
 from subprocess import PIPE, Popen
+from threading import Thread
+from Queue import Queue, Empty  # Python 2.x
 
-
+ON_POSIX = 'posix' in sys.builtin_module_names
 JOSHUA_PATH = os.environ['JOSHUA']
-my_unix_command = os.path.join(JOSHUA_PATH, 'joshua-decoder')
+JOSHUA_CMD = os.path.join(JOSHUA_PATH, 'joshua-decoder')
 HOST = 'localhost'
 PORT = 2000
 
 
-def pipe_command(arg_list, standard_input=False):
-    "arg_list is [command, arg1, ...], standard_input is string"
-    pipe = PIPE if standard_input else None
-    subp = Popen(arg_list, stdin=pipe, stdout=PIPE)
-    if not standard_input:
-        return subp.communicate()[0]
-    return subp.communicate(standard_input)[0]
+class JoshuaDecoderSubprocess(object):
+
+    def _enqueue_output(self, out, queue):
+        for line in iter(out.readline, b''):
+            queue.put(line)
+        out.close()
+
+    def __init__(self, joshua_args):
+        cmd = [JOSHUA_CMD] + joshua_args
+        self.proc = Popen(cmd, stdin=PIPE, stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
+
+        # Use Queue and Thread to read output without blocking.
+        self.q_stdout = Queue()
+        self.t_stdout = Thread(target=self._enqueue_output,
+                               args=(self.proc.stdout, self.q_stdout))
+        self.t_stdout.daemon = True  # thread dies with the program
+        self.t_stdout.start()
+
+    def translate(self, input_text):
+        """Process the input text with the Joshua decoder"""
+        self.proc.stdin.write(input_text.encode('utf-8'))
+        # Read the decoded output.
+        # Read line without blocking.
+        while True:
+            try:
+                return self.q_stdout.get().decode('utf-8')
+            except Empty:
+                print('no output yet')
+
+
+# Dirty, dirty global object.
+JOSHUA_SUBPROCESS = JoshuaDecoderSubprocess(sys.argv[1:])
 
 
 class SingleTCPHandler(SocketServer.BaseRequestHandler):
@@ -31,7 +73,7 @@ class SingleTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the client connection
         data = self.request.recv(1024)  # clip input at 1Kb
-        reply = pipe_command(my_unix_command, data)
+        reply = JOSHUA_SUBPROCESS.translate(data)
         if reply is not None:
             self.request.send(reply)
         self.request.close()
@@ -45,6 +87,7 @@ class SimpleServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
     def __init__(self, server_address, RequestHandlerClass):
         SocketServer.TCPServer.__init__(self, server_address, RequestHandlerClass)
+
 
 if __name__ == "__main__":
     server = SimpleServer((HOST, PORT), SingleTCPHandler)
