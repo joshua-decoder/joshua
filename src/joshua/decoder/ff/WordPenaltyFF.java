eceefecc183258cc8f8e276323b81ca195db961b
Matt Post <post@cs.jhu.edu>
2012-09-05 16:57:04 +0100

    moved feature functions to new interface

--- src/joshua/decoder/ff/WordPenaltyFF.java
+++ src/joshua/decoder/ff/WordPenaltyFF.java
@@ -1,18 +1,3 @@
-/*
- * This file is part of the Joshua Machine Translation System.
- * 
- * Joshua is free software; you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License along with this library;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA
- */
 package joshua.decoder.ff;
 
 import joshua.decoder.ff.tm.Rule;
@@ -20,30 +5,43 @@ import joshua.decoder.ff.tm.Rule;
 
 /**
  * 
- * @author Zhifei Li, <zhifei.work@gmail.com>
- * @version $LastChangedDate$
+ * @author Zhifei Li <zhifei.work@gmail.com>
+ * @author Matt Post <post@cs.jhu.edu>
  */
-public final class WordPenaltyFF extends DefaultStatelessFF {
+public final class WordPenaltyFF extends StatelessFF {
 
-  private static final double OMEGA = -Math.log10(Math.E);// -0.435
+  private static final float OMEGA = -Math.log10(Math.E); // -0.435
 
-  public WordPenaltyFF(int featureID, double weight) {
-    super(weight, -1, featureID); // TODO: owner
+  /* This is a single-value feature template, so we cache the weight here.
+   */
+  private float weight;
+
+  public WordPenaltyFF(final FeatureVector weights, String args) {
+    super(weights, "WordPenaltyFF", args);
+
+    // Find the weight for this feature in the weights hash and cache it.
+    if (weights.containsKey(name)) {
+      weight = weights.get(name);
+    } else {
+      logger.warn("* WARNING: no weight for feature '" + name + "'");
+      weight = 0.0;
+    }
+  }
+
+  /* Each word in each rule incurs a penalty of OMEGA.  So we compute the number of words and
+   * multiply it by OMEGA.
+   *
+   * I'm not sure why it doesn't just incur a penalty of one.
+   */
+  public FeatureVector computeFeatures(Rule rule, SourcePath sourcePath, int sentID) {
+    return new FeatureVector(name, OMEGA * (rule.getEnglish().length - rule.getArity()));
   }
 
   /**
-   * Each additional word gets a penalty. The more number of words, the more negative. So, to
-   * encourage longer sentence, we should have a negative weight on the feature
+   * Compute the cost directly instead of chaining to computeFeatures, since we have just one
+   * weight.
    */
-  public double estimateLogP(final Rule rule, int sentID) {
-    // we do not check for owner because we want this feature used for all the time, e.g., under
-    // oov_owner case
-    // TODO: why not check the owner
-    /*
-     * if (this.owner == rule.owner) { return OMEGA * (rule.english.length - rule.arity); } else {
-     * return 0.0; }
-     */
-
-    return OMEGA * (rule.getEnglish().length - rule.getArity());
+  public float computeCost(Rule rule, SourcePath sourcePath, int sentID) {
+    return weight * OMEGA * (rule.getEnglish().length - rule.getArity());
   }
 }
