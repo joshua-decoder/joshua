6e912b6b7ee5cbedfa0a7d56cb767c4089ad87fc
Matt Post <post@cs.jhu.edu>
2012-09-06 15:18:11 +0100

    put OOV feature on the new feature interface

--- src/joshua/decoder/ff/OOVFF.java
+++ src/joshua/decoder/ff/OOVFF.java
@@ -1,42 +1,30 @@
-/*
- * This file is part of the Joshua Machine Translation System.
- * 
- * Joshua is free software; you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License along with this library;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA
- */
 package joshua.decoder.ff;
 
 import joshua.decoder.ff.tm.AbstractGrammar;
 import joshua.decoder.ff.tm.Rule;
 
-/**
- * 
- * @author Matt Post <post@jhu.edu>
- * @version $LastChangedDate$
- */
-public final class OOVFF extends DefaultStatelessFF {
+import joshua.decoder.chart_parser.SourcePath;
 
-  public OOVFF(int featureID, double weight, int owner) {
-    super(weight, owner, featureID); // TODO: owner
+/**
+ * This feature is fired when an out-of-vocabulary word (with respect to the translation model) is
+ * entered into the chart.
+ *
+ * @author Matt Post <post@cs.jhu.edu>
+ */
+public final class OOVFF extends StatelessFF {
+
+  public OOVFF(FeatureVector weights) {
+    super(weights, "OOVpenalty");
   }
 
   /**
    * Each additional word gets a penalty. The more number of words, the more negative. So, to
    * encourage longer sentence, we should have a negative weight on the feature
    */
-  public double estimateLogP(final Rule rule, int sentID) {
+  public float computeCost(final Rule rule, final SourcePath sourcePath, int sentID) {
     if (rule.getRuleID() == AbstractGrammar.OOV_RULE_ID)
-      return 1.0;
+      return 1.0f;
     else
-      return 0.0;
+      return 0.0f;
   }
 }
