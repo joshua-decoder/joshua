8a355f5d120a056362800fc2d186c911ba667dac
Matt Post <post@cs.jhu.edu>
2012-09-06 18:16:01 +0100

    removed state ID functions from interface

--- src/joshua/decoder/ff/state_maintenance/StateComputer.java
+++ src/joshua/decoder/ff/state_maintenance/StateComputer.java
@@ -8,13 +8,6 @@ import joshua.decoder.hypergraph.HGNode;
 
 public interface StateComputer<D extends DPState> {
 
-  /*
-   * a valid stateID can be any integer except -1
-   */
-  void setStateID(int stateID);
-
-  int getStateID();
-
   D computeState(Rule rule, List<HGNode> antNodes, int spanStart, int spanEnd, SourcePath srcPath);
 
   D computeFinalState(HGNode antNode, int spanStart, int spanEnd, SourcePath srcPath);
