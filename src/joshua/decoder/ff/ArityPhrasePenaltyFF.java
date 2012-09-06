bd5ac8e03bd73bd609627bab6ee0a8552da8ea60
Matt Post <post@cs.jhu.edu>
2012-09-06 18:13:28 +0100

    fixed bugs preventing compilation

--- src/joshua/decoder/ff/ArityPhrasePenaltyFF.java
+++ src/joshua/decoder/ff/ArityPhrasePenaltyFF.java
@@ -4,6 +4,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import joshua.decoder.ff.tm.Rule;
+import joshua.decoder.chart_parser.SourcePath;
+import joshua.corpus.Vocabulary;
 
 /**
  * This feature function counts rules from a particular grammar (identified by the owner) having an
@@ -20,20 +22,26 @@ public final class ArityPhrasePenaltyFF extends StatelessFF {
   private final int minArity;
   private final int maxArity;
 
+	// Cache the weight from the weight vector;
+	private float weight;
+
   public ArityPhrasePenaltyFF(final FeatureVector weights, String argString) {
-    super(weight, "ArityPenalty", argString);
+    super(weights, "ArityPenalty", argString);
 
     // Process the args for the owner, minimum, and maximum.  
     
     // TODO: This should be done in a general way by FeatureFunction::processArgs, in a way that
     // allows any feature to have arguments.
-    String args[] = argString.split();
+    String args[] = argString.split("\\s+");
     this.owner = Vocabulary.id(args[0]);
     this.minArity = Integer.parseInt(args[1]);
     this.maxArity = Integer.parseInt(args[2]);
 
-    logger.fine(String.format("ArityPhrasePenaltyFF feature with owner=%s, minArity=%d, maxArity=%d", owner, minArity, maxArity));
-  }
+		if (! weights.containsKey(name))
+			System.err.println("WARNING: no weight found for feature '" + name + "'");
+
+		this.weight = weights.get(name);
+ }
 
   /**
    * Returns 1 if the arity penalty feature applies to the current rule
@@ -46,9 +54,8 @@ public final class ArityPhrasePenaltyFF extends StatelessFF {
   }
 
   public FeatureVector computeFeatures(Rule rule, SourcePath sourcePath, int sentID) {
-    return FeatureVector(name, isEligible(rule));
+    return new FeatureVector(name, isEligible(rule));
   }
-
 
   public float computeCost(Rule rule, int SentID) {
     return weight * isEligible(rule);
