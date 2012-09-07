28ae246a9d7c0f9f630327392245c90736785dec
Matt Post <post@cs.jhu.edu>
2012-09-07 12:08:48 +0100

    moved StatelessQuantizer interface to its own file

---
+++ src/joshua/util/quantization/StatelessQuantizer.java
@@ -0,0 +1,24 @@
+package joshua.util.quantization;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author Juri Ganitkevitch <juri@cs.jhu.edu>
+ */
+abstract class StatelessQuantizer implements Quantizer {
+
+  public void initialize() {}
+
+  public void add(float key) {}
+
+  public void finalize() {}
+
+  public void writeState(DataOutputStream out) throws IOException {
+    out.writeUTF(getKey());
+  }
+
+  public void readState(DataInputStream in) throws IOException {}
+}
