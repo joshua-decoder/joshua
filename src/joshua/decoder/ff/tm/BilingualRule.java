b4622a1ce45ad9269f0818aefd049be82a120952
Matt Post <post@cs.jhu.edu>
2012-09-21 21:27:26 -0400

    bugfix: toString() printed out wrong values for english-side nonterminals

--- src/joshua/decoder/ff/tm/BilingualRule.java
+++ src/joshua/decoder/ff/tm/BilingualRule.java
@@ -1,5 +1,6 @@
 package joshua.decoder.ff.tm;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -23,8 +24,8 @@ public class BilingualRule extends MonolingualRule {
   /**
    * Constructs a new rule using the provided parameters. The owner and rule id for this rule are
    * undefined. Note that some of the sparse features may be unlabeled, but they cannot be mapped to
-   * their default names ("tm_OWNER_INDEX") until later, when we know the owner of the
-   * rule. This is not known until the rule is actually added to a grammar in Grammar::addRule().
+   * their default names ("tm_OWNER_INDEX") until later, when we know the owner of the rule. This is
+   * not known until the rule is actually added to a grammar in Grammar::addRule().
    * 
    * @param lhs Left-hand side of the rule.
    * @param sourceRhs Source language right-hand side of the rule.
@@ -58,19 +59,37 @@ public class BilingualRule extends MonolingualRule {
     return this.english;
   }
 
-  // ===============================================================
-  // Serialization Methods
-  // ===============================================================
-  // TODO: remove these methods
+  /**
+   * The nonterminals on the English side are pointers to the source side nonterminals (-1 and -2),
+   * rather than being directly encoded. These number indicate the correspondence between the
+   * nonterminals on each side, introducing a level of indirection however when we want to resolve
+   * them. So to get the ID, we need to look up the corresponding source side ID.
+   * 
+   * @return The string of English words
+   */
+  public String getEnglishWords() {
+    ArrayList<String> foreignNTs = new ArrayList<String>();
+    for (int i = 0; i < this.getFrench().length; i++)
+      if (this.getFrench()[i] < 0) foreignNTs.add(Vocabulary.word(this.getFrench()[i]));
 
-  // Caching this method significantly improves performance
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < this.getEnglish().length; i++) {
+      if (this.getEnglish()[i] >= 0)
+        sb.append(Vocabulary.word(this.getEnglish()[i]) + " ");
+      else
+        sb.append(foreignNTs.get(Math.abs(this.getEnglish()[i]) - 1) + "," + Math.abs(this.getEnglish()[i]) + " ");
+    }
+
+    return sb.toString().trim();
+  }
+
   public String toString() {
     StringBuffer sb = new StringBuffer();
     sb.append(Vocabulary.word(this.getLHS()));
     sb.append(" ||| ");
     sb.append(Vocabulary.getWords(this.getFrench()));
     sb.append(" ||| ");
-    sb.append(Vocabulary.getWords(this.getEnglish()));
+    sb.append(getEnglishWords());
     sb.append(" |||");
     sb.append(" " + getFeatureVector());
     sb.append(String.format(" ||| %.3f", getEstimatedCost()));
