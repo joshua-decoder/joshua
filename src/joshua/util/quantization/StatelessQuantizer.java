dffa0b2c3b163b39399bb3fc034f8d038ec7720a
Matt Post <post@cs.jhu.edu>
2012-10-09 14:49:34 -0400

    split out StatelessQuantizer which could not be found on OS X

---
+++ src/joshua/util/quantization/StatelessQuantizer.java
@@ -0,0 +1,21 @@
+package joshua.util.quantization;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+abstract class StatelessQuantizer implements Quantizer {
+
+  public void initialize() {}
+
+  public void add(float key) {}
+
+  public void finalize() {}
+
+  public void writeState(DataOutputStream out) throws IOException {
+    out.writeUTF(getKey());
+  }
+
+  public void readState(DataInputStream in) throws IOException {}
+}
