829e2e74dc2f4ae7ca7719b0ff5666cac9cd591d
Matt Post <post@cs.jhu.edu>
2012-10-18 14:35:54 -0400

    bugfix in passing iteration to decoder (thanks to Kenneth Heafield)

--- src/joshua/pro/PROCore.java
+++ src/joshua/pro/PROCore.java
@@ -1,19 +1,3 @@
-/*
- * This file is part of the Joshua Machine Translation System.
- * 
- * Joshua is free software; you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License along with this library;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA
- */
-
 package joshua.pro;
 
 import java.io.BufferedReader;
@@ -1747,9 +1731,11 @@ public class PROCore {
       println("Running external decoder...", 1);
 
       try {
-        String cmd = decoderCommandFileName;
+        ArrayList<String> cmd = new ArrayList<String>();
+        cmd.add(decoderCommandFileName);
+
         if (passIterationToDecoder)
-          cmd = cmd + " " + iteration;
+          cmd.add(Integer.toString(iteration));
 
         ProcessBuilder pb = new ProcessBuilder(cmd);
         // this merges the error and output streams of the subprocess
