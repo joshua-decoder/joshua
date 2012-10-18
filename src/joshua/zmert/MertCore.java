e879bd87760fdda05bbf39f54439655c1bde1fa0
Matt Post <post@cs.jhu.edu>
2012-10-18 14:48:23 -0400

    bugfix in passing iteration to sub-commands (thanks to Kenneth Heafield)

--- src/joshua/zmert/MertCore.java
+++ src/joshua/zmert/MertCore.java
@@ -1,19 +1,3 @@
-/*
- * This file is part of the Joshua Machine Translation System.
- * 
- * Joshua is free software; you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License along with this library;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA
- */
-
 package joshua.zmert;
 
 import java.io.BufferedReader;
@@ -33,6 +17,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
@@ -1443,9 +1428,10 @@ public class MertCore {
       println("Running external decoder...", 1);
 
       try {
-        String cmd = decoderCommandFileName;
+        ArrayList<String> cmd = new ArrayList<String>();
+        cmd.add(decoderCommandFileName);
         if (passIterationToDecoder)
-          cmd += " " + Integer.toString(iteration);
+          cmd.add(Integer.toString(iteration)
 
         ProcessBuilder pb = new ProcessBuilder(cmd);
         // this merges the error and output streams of the subprocess
