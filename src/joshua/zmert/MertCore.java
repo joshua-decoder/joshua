c0ef0ea17688f2b7303e6160b72406493d9c5637
Matt Post <post@cs.jhu.edu>
2012-10-18 21:03:43 -0400

    fixed compilation error

--- src/joshua/zmert/MertCore.java
+++ src/joshua/zmert/MertCore.java
@@ -1431,7 +1431,7 @@ public class MertCore {
         ArrayList<String> cmd = new ArrayList<String>();
         cmd.add(decoderCommandFileName);
         if (passIterationToDecoder)
-          cmd.add(Integer.toString(iteration)
+          cmd.add(Integer.toString(iteration));
 
         ProcessBuilder pb = new ProcessBuilder(cmd);
         // this merges the error and output streams of the subprocess
