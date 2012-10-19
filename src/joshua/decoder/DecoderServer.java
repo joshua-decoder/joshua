b9bdcd20f9bf0e1ea0943f0fe1fef939ed919b7f
Luke Orland <orluke@gmail.com>
2012-10-19 14:32:50 -0400

    only instantiate one JoshuaDecoder,
    instead of a new one with every GET request

--- src/joshua/decoder/DecoderServer.java
+++ src/joshua/decoder/DecoderServer.java
@@ -1,8 +1,6 @@
 package joshua.decoder;
 
 import java.io.IOException;
-import java.util.Iterator;
-
 import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.resource.Get;
@@ -11,20 +9,29 @@ import org.restlet.resource.ServerResource;
 
 public class DecoderServer extends ServerResource {
 
+  private final JoshuaDecoder decoder;
+
+  public DecoderServer() {
+    String configFile = null;
+    this.decoder = new JoshuaDecoder(configFile);
+  }
+
   public static void main(String[] args) throws Exception {
+
     // Create the HTTP server and listen on port 8182
     new Server(Protocol.HTTP, 8182, DecoderServer.class).start();
   }
 
+  @Override
   @Get
   public String toString() {
-    String configFile = null;
+
     String testFile = "hello_world_file";
     String nbestFile = "-";
     String oracleFile = null;
-    JoshuaDecoder decoder = new JoshuaDecoder(configFile);
+
     try {
-      decoder.decodeTestSet(testFile, nbestFile, oracleFile);
+      this.decoder.decodeTestSet(testFile, nbestFile, oracleFile);
     } catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
