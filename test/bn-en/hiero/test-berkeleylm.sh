79f12ca167fee57be3d2698a9ee31f6ee0744f89
Matt Post <post@cs.jhu.edu>
2012-11-04 22:36:05 -0500

    updated berkeleylm test case

--- test/bn-en/hiero/test-berkeleylm.sh
+++ test/bn-en/hiero/test-berkeleylm.sh
@@ -2,18 +2,21 @@
 
 set -u
 
-cat input.bn | $JOSHUA/joshua-decoder -m 1g -threads 2 -c joshua-berkeleylm.config > output 2> log
+cat input.bn | $JOSHUA/bin/joshua-decoder -m 1g -threads 2 -c joshua-berkeleylm.config > output 2> log
 
-java -cp $JOSHUA/bin -Dfile.encoding=utf8 -Djava.library.path=lib -Xmx256m -Xms256m -Djava.util.logging.config.file=logging.properties joshua.util.JoshuaEval -cand output -format nbest -ref reference.en -rps 4 -m BLEU 4 closest > output.bleu
+# Extract the translations and model scores
+cat output | awk -F\| '{print $4 " ||| " $10}' > output.scores
 
-diff -u output.bleu output.gold.bleu > diff
+# Compare
+diff -u output.scores gold.scores.berkeleylm > diff
 
 if [ $? -eq 0 ]; then
 	echo PASSED
+	rm -f output log output.scores diff
 	exit 0
 else
 	echo FAILED
-	cat diff
+	tail diff
 	exit 1
 fi
 
