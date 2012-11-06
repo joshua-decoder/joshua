1a109f7e6ecdaa1cb0b48dc3aab145284e9f7c70
Luke Orland <orluke@gmail.com>
2012-11-05 21:33:03 -0500

    TranslationRequest and Translations tests compiling and passing

--- test/joshua/decoder/io/TranslationRequestTest.java
+++ test/joshua/decoder/io/TranslationRequestTest.java
@@ -2,8 +2,6 @@ package joshua.decoder.io;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.NoSuchElementException;
-
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import static org.mockito.Mockito.*;
@@ -95,132 +93,6 @@ public class TranslationRequestTest {
   }
 
   /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#hasNext()}.
-   */
-  @Test(enabled = true)
-  public void testHasNext_emptyInput() {
-    byte[] data = "".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertFalse(request.hasNext());
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#hasNext()}.
-   */
-  @Test(enabled = true)
-  public void testHasNext_inputNotEndingWithNewline() {
-    byte[] data = "1".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertTrue(request.hasNext());
-    // Remove the next
-    request.next();
-    // Should be empty
-    assertFalse(request.hasNext());
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#hasNext()}.
-   */
-  @Test(enabled = true)
-  public void testHasNext_inputEndingWithNewline() {
-    byte[] data = "1\n".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertTrue(request.hasNext());
-    // Remove the next
-    request.next();
-    // Should be empty
-    assertFalse(request.hasNext());
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#hasNext()}.
-   */
-  @Test(enabled = true)
-  public void testHasNext_2InputsEndingWithNewline() {
-    byte[] data = "1\n2\n".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertTrue(request.hasNext());
-    // Remove the next two.
-    request.next();
-    request.next();
-    // Should be empty
-    assertFalse(request.hasNext());
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#hasNext()}.
-   */
-  @Test(enabled = true)
-  public void testHasNext_whitespace() {
-    byte[] data = " ".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertTrue(request.hasNext());
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#hasNext()}.
-   */
-  @Test(enabled = true)
-  public void testHasNext_whitespaceNewline() {
-    byte[] data = " \n".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertTrue(request.hasNext());
-    request.next();
-    assertFalse(request.hasNext());
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#next()}.
-   */
-  @Test(enabled = true)
-  public void testNext_1() {
-    byte[] data = "1\n".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertEquals(request.next().source(), "1");
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#next()}.
-   */
-  @Test(expectedExceptions = NoSuchElementException.class)
-  public void testNext_empty() {
-    byte[] data = "".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    request.next();
-    // Exception should have been thrown.
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#next()}.
-   */
-  @Test(enabled = true)
-  public void testNext_newline() {
-    byte[] data = "\n".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertEquals(request.next().source(), "");
-  }
-
-  /**
-   * Test method for {@link joshua.decoder.io.TranslationRequest#next()}.
-   */
-  @Test(enabled = true)
-  public void testNext_whitespaceNewline() {
-    byte[] data = " \n".getBytes();
-    ByteArrayInputStream input = new ByteArrayInputStream(data);
-    TranslationRequest request = new TranslationRequest(input);
-    assertEquals(request.next().source(), "");
-  }
-
-  /**
    * Test method for {@link joshua.decoder.io.TranslationRequest#next()}.
    */
   @Test(enabled = true)
@@ -230,7 +102,6 @@ public class TranslationRequestTest {
     TranslationRequest request = new TranslationRequest(input);
     assertEquals(request.next().source(), "");
     assertEquals(request.next().source(), "");
-    assertFalse(request.hasNext());
   }
 
   /**
