e83ef460a34d525da402cab3cf11eacee5680b1a
Matt Post <post@cs.jhu.edu>
2012-11-08 12:08:43 -0500

    updated regexp test case to work with new format

--- test/decoder/regexp-grammar/test.sh
+++ test/decoder/regexp-grammar/test.sh
@@ -2,16 +2,17 @@
 
 set -u
 
-cat input | $JOSHUA/bin/joshua-decoder -c config > output 2> /dev/null
+cat input | $JOSHUA/bin/joshua-decoder -c config > output 2> log
 
 diff -u output output.gold > diff
 
 if [ $? -eq 0 ]; then
 	echo PASSED
+  rm -rf output log diff
 	exit 0
 else
 	echo FAILED
-	cat diff
+	tail diff
 	exit 1
 fi
 
