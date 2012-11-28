1394d5ffe0b2dbdce7da6676e4d93730c339723c
Matt Post <post@cs.jhu.edu>
2012-11-28 15:29:44 -0500

    bugfix: now handles empty hypotheses

--- src/joshua/decoder/NbestMinRiskReranker.java
+++ src/joshua/decoder/NbestMinRiskReranker.java
@@ -1,19 +1,3 @@
-/*
- * This file is part of the Joshua Machine Translation System.
- * 
- * Joshua is free software; you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License along with this library;
- * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA
- */
-
 package joshua.decoder;
 
 import java.io.IOException;
@@ -41,7 +25,6 @@ import joshua.util.Regex;
  * if one want to deal with spurious ambiguity, he/she should do that before calling this class
  * 
  * @author Zhifei Li, <zhifei.work@gmail.com>
- * @version $LastChangedDate$
  */
 public class NbestMinRiskReranker {
 
@@ -71,6 +54,15 @@ public class NbestMinRiskReranker {
     // step-0: preprocess
     // assumption: each hyp has a formate:
     // "sent_id ||| hyp_itself ||| feature scores ||| linear-combination-of-feature-scores(this should be logP)"
+
+    /* Quit if you find an empty hypothesis. */
+    if (nbest.size() == 1) {
+      String[] fields = Regex.threeBarsWithSpace.split(nbest.get(0));
+      if (fields[1].equals("") || Regex.spaces.matches(fields[1])) {
+        System.err.println(String.format("-> sentence is empty"));
+        return "";
+      }
+    } 
 
     List<String> hypsItself = new ArrayList<String>();
     // ArrayList<String> l_feat_scores = new ArrayList<String>();
