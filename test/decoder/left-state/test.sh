db987f70e1222f48047db2a3840e3436954030bf
Matt Post <post@cs.jhu.edu>
2013-07-15 16:02:38 -0400

    added test-case for left-state minimization

---
+++ test/decoder/left-state/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -u
+
+cat input.bn | $JOSHUA/bin/joshua-decoder -m 1g -threads 2 -c joshua.config > output 2> log
+
+# Extract the translations and model scores
+cat output | awk -F"\|" '{print $1 "|||" $4 "|||" $10}' > output.scores
+
+# Compare
+diff -u output.scores output.gold.scores > diff
+
+if [ $? -eq 0 ]; then
+  echo PASSED
+  rm -f diff output log output.scores
+  exit 0
+else
+  echo FAILED
+  tail diff
+  exit 1
+fi
+
+
