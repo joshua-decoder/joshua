eef78109e8b55158c95b9c4e0a421ab51ba8f0c1
Matt Post <post@cs.jhu.edu>
2013-08-16 16:22:33 -0400

    Cleaned up some of the examples

--- examples/parser/parse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-java  -classpath ${CLASSPATH}:${JOSHUA}/bin \
-      -Djava.library.path=${JOSHUA}/lib \
-      -Dfile.encoding=utf8 \
-      -Djava.util.logging.config.file=${JOSHUA}/logging.properties \
-      -Xmx55g -Xms55g \
-      joshua.decoder.JoshuaDecoder $CONF <$INPUT 2>$LOG
