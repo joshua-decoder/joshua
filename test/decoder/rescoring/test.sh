c7f6ed876852f6f25b8da235944fc4f6a32555b7
Matt Post <post@cs.jhu.edu>
2013-09-13 18:55:31 +0200

    Updated test cases (changes caused by rounding errors)

---
+++ test/decoder/rescoring/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -u
+
+cat input.bn | $JOSHUA/bin/joshua-decoder -m 1g -threads 1 -c joshua.config > output 2> log
+
+# Compare
+diff -u output output.gold > diff
+
+if [ $? -eq 0 ]; then
+	echo PASSED
+	rm -f diff log output output.scores
+	exit 0
+else
+	echo FAILED
+	tail diff
+	exit 1
+fi
