0c29ca75f868adff82afc8b89d1be67e45eb87d5
Matt Post <post@cs.jhu.edu>
2013-09-13 18:44:53 +0200

    Removed rescore() function and unused imports

--- src/joshua/decoder/hypergraph/HyperGraph.java
+++ src/joshua/decoder/hypergraph/HyperGraph.java
@@ -6,12 +6,9 @@ import java.io.FileWriter;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Stack;
 import java.util.logging.Logger;
-
-import joshua.decoder.BLEU;
 
 /**
  * this class implement (1) HyperGraph-related data structures (Item and Hyper-edges)
@@ -97,43 +94,5 @@ public class HyperGraph {
 
   public float bestScore() {
     return this.goalNode.bestHyperedge.getBestDerivationScore();
-  }
-
-  /**
-   * Rescore a forest using BLEU score.
-   * 
-   * This is a somewhat complicated endeavor. What we want is to have every edge in the forest
-   * reflect a weighted score between (a) the original model score and (b) a BLEU approximation.
-   * 
-   * This function walks the hyperforest in a depth-first, post-order traversal. This provides us
-   * with the necessary CKY guarantee that the antecedents of each node will have been updated
-   * before the node itself.
-   * 
-   * The update does a few things:
-   * 
-   * <ol>
-   * <li> For each hyperedge, we compute the BLEU statistics incurred by applying that edge. Together 
-   * with the BLEU statistics from the tail nodes, we can compute a BLEU score, which is recorded
-   * for that hyperedge.
-   * <li> It is
-   * 
-   * @param corpusStats
-   */
-  public void rescore(BLEU.Stats corpusStats, float bleuWeight) {
-    final HashMap<HGNode, BLEU.Stats> bleuStatsHash = new HashMap<HGNode, BLEU.Stats>();
-
-    ForestWalker walker = new ForestWalker(ForestWalker.TRAVERSAL.POSTORDER);
-    walker.walk(this.goalNode, new WalkerFunction() {
-      public void apply(HGNode node) {
-        for (HyperEdge edge : node.getHyperEdges()) {
-          BLEU.Stats stats = BLEU.compute(edge.getRule());
-          if (edge.getTailNodes() != null) {
-            for (HGNode tailNode : edge.getTailNodes()) {
-              stats.add(bleuStatsHash.get(tailNode));
-            }
-          }
-        }
-      }
-    });
   }
 }
