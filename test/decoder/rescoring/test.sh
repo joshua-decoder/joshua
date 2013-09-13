21a8ef69906e1c5a54e2fc2fa25711e949b25520
Matt Post <post@cs.jhu.edu>
2013-09-13 19:09:30 +0200

    Fixed rescoring test case

--- test/decoder/rescoring/test.sh
+++ test/decoder/rescoring/test.sh
@@ -2,14 +2,14 @@
 
 set -u
 
-cat input.bn | $JOSHUA/bin/joshua-decoder -m 1g -threads 1 -c joshua.config > output 2> log
+cat input.txt | $JOSHUA/bin/joshua-decoder -m 1g -threads 1 -c joshua.config > output 2> log
 
 # Compare
 diff -u output output.gold > diff
 
 if [ $? -eq 0 ]; then
 	echo PASSED
-	rm -f diff log output output.scores
+	rm -f diff log output 
 	exit 0
 else
 	echo FAILED
