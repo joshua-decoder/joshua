bf52a8ad1b22a2b334e44a11c7c3226d3f33ad6f
Gideon Wenniger <gideon.wenniger@domain.com>
2013-10-03 16:42:50 +0200

    Gideon : Implemented a smarter way to do the matching of alternate nonterminals for soft syntactic constraint decoding. As it turns out, naive matching based on looping over all child non-terminals in a children table from the Trie, is just too expensive for reasonable size grammars. In fact the best would be split the Trie in two parts: one for children that are non-terminals and one for children that are terminals. This would be the most general solution. This is an expensive solution only in that is requires a thorough refactoring of the rule table code.
    As an intermediate solution we implemented first something now that works well for
    small sets of Nonterminals. Rather than looping over all children and checking for
    each if it is a nonterminal, we rather get a fixed set of present nonterminals
    (except for OOV and Goal) in the grammar, and use those to directly get those
    alternate nonterminals if present using the 'containsKey' and 'get' method of HashMap.
    This is still relatively very cheap compared to the old alternative, provided the
    number of Nonterminals is small. If the latter is not the case this solution will  not
    work, we need to refactor the code and make an efficient method getNonterminalChildren
    in the Trie implementation to deal with the more general case.

--- src/joshua/decoder/chart_parser/NonterminalMatcherFactory.java
+++ src/joshua/decoder/chart_parser/NonterminalMatcherFactory.java
@@ -22,45 +22,123 @@ import joshua.decoder.ff.tm.Trie;
  */
 public class NonterminalMatcherFactory {
 
+  /**
+   * How much nonterminals there may be maximal to use targeted querying rather than matching to
+   * find the alternate nonterminals from the children table when doing soft syntactic translation.
+   * Note that there is a tradeoff here: looping over all children and matching is rather is
+   * expensive if there are many children, which is typically the case for big grammars as there are
+   * many possible words at each level in the Trie. Targeted querying is only cheap provided the
+   * number of nonterminals is small, otherwise it may in fact become more expensive than just
+   * looping and matching.
+   */
+  private static final int MAX_TOTAL_NON_TERMINALS_FOR_TARGETED_QUERYING = 1000;
+
+  private static String OOV_LABEL = "[OOV]";
+
+  protected static boolean isOOVLabelOrGoalLabel(String label,
+      JoshuaConfiguration joshuaConfiguration) {
+    return (label.equals(OOV_LABEL) || label.equals(joshuaConfiguration.goal_symbol));
+  }
+
+  private static boolean useTargetdQuerying(List<Integer> nonterminalIndicesExceptForGoalAndOOV) {
+    if (nonterminalIndicesExceptForGoalAndOOV.size() <= MAX_TOTAL_NON_TERMINALS_FOR_TARGETED_QUERYING) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * This method returns a list of all indices corresponding to Nonterminals in the Vocabulary
+   * 
+   * @return
+   */
+  public static List<Integer> getAllNonterminalIndicesExceptForGoalAndOOV(
+      JoshuaConfiguration joshuaConfiguration) {
+    List<Integer> result = new ArrayList<Integer>();
+    List<Integer> nonterminalIndices = Vocabulary.getNonterminalIndices();
+    for (Integer nonterminalIndex : nonterminalIndices) {
+      if (!isOOVLabelOrGoalLabel(Vocabulary.word(nonterminalIndex), joshuaConfiguration)) {
+        result.add(nonterminalIndex);
+      }
+    }
+    return result;
+  }
+
   public static NonterminalMatcher createNonterminalMatcher(Logger logger,
       JoshuaConfiguration joshuaConfiguration) {
+    List<Integer> allNonterminalIndicesExceptForGoalAndOOV = getAllNonterminalIndicesExceptForGoalAndOOV(joshuaConfiguration);
+
+    if (allNonterminalIndicesExceptForGoalAndOOV.isEmpty()) {
+      throw new RuntimeException(
+          "Error: NonterminalMatcherFactory. createNonterminalMatcher -  empty nonterminal indices table");
+    }
+
     if (joshuaConfiguration.softSyntacticConstraintDecoding) {
-      return new StandardNonterminalMatcherSoftConstraints(logger, joshuaConfiguration);
+      return new StandardNonterminalMatcherSoftConstraints(logger, joshuaConfiguration,
+          allNonterminalIndicesExceptForGoalAndOOV,
+          useTargetdQuerying(allNonterminalIndicesExceptForGoalAndOOV));
     } else {
-      return new StandardNonterminalMatcherStrict(logger, joshuaConfiguration);
+      return new StandardNonterminalMatcherStrict(logger, joshuaConfiguration,
+          allNonterminalIndicesExceptForGoalAndOOV,
+          useTargetdQuerying(allNonterminalIndicesExceptForGoalAndOOV));
     }
   }
 
   protected abstract static class AbstractNonterminalMatcher implements NonterminalMatcher {
+    // A list of nonTerminalIndices, to be used for faster retrieval of Nonterminals in
+    // soft syntactic matching
+    private final List<Integer> nonterminalIndicesExceptForGoalAndOOV;
 
-    private static String OOV_LABEL = "[OOV]";
     protected final Logger logger;
     protected final JoshuaConfiguration joshuaConfiguration;
+    private final boolean useTargetQueryingToCollectAlternateNonterminals;
 
-    protected AbstractNonterminalMatcher(Logger logger, JoshuaConfiguration joshuaConfiguration) {
+    protected AbstractNonterminalMatcher(Logger logger, JoshuaConfiguration joshuaConfiguration,
+        List<Integer> nonterminalIndicesExceptForGoalAndOOV,
+        boolean useTargetQueryingToCollectAlternateNonterminals) {
       this.logger = logger;
       this.joshuaConfiguration = joshuaConfiguration;
+      this.nonterminalIndicesExceptForGoalAndOOV = nonterminalIndicesExceptForGoalAndOOV;
+      this.useTargetQueryingToCollectAlternateNonterminals = useTargetQueryingToCollectAlternateNonterminals;
     }
 
-    private boolean isNonterminal(int wordIndex) {
+    private static boolean isNonterminal(int wordIndex) {
       return wordIndex < 0;
     }
 
-    protected boolean isOOVLabelOrGoalLabel(String label) {
-      return (label.equals(OOV_LABEL) || label.equals(joshuaConfiguration.goal_symbol));
+    /**
+     * This method finds Nonterminal entries from the children in the Children HashMap using a
+     * targeted querying strategy, based on knowledge of what the Nonterminals are. Storing the
+     * Nonterminals and Terminals in the Trie separately would be an even smarter strategy perhaps,
+     * but requires a more thorough refactoring of the code
+     * 
+     * @param childrenTbl
+     * @return
+     */
+    private List<Trie> getNonTerminalsListFromChildrenByTargetedQuerying(
+        HashMap<Integer, ? extends Trie> childrenTbl) {
+      List<Trie> trieList = new ArrayList<Trie>();
+
+      if (childrenTbl != null) {
+        // get all the extensions, map to string, check for *, build regexp
+
+        for (Integer index : this.nonterminalIndicesExceptForGoalAndOOV) {
+          String wordIdWord = Vocabulary.word(index);
+          logger.info(">>>>>>>>>>>>wordIDWord: " + wordIdWord);
+
+          int nonterminalIndexTrieFormat = -index;
+          if (childrenTbl.containsKey(nonterminalIndexTrieFormat)) {
+            trieList.add(childrenTbl.get(nonterminalIndexTrieFormat));
+          }
+        }
+      }
+      return trieList;
+
     }
 
-    protected List<Trie> matchAllEqualOrBothNonTerminalAndNotGoalOrOOV(DotNode dotNode, int wordID) {
+    private List<Trie> getNonTerminalsListFromChildrenByMatching(
+        HashMap<Integer, ? extends Trie> childrenTbl, int wordID) {
       List<Trie> trieList = new ArrayList<Trie>();
-      HashMap<Integer, ? extends Trie> childrenTbl = dotNode.getTrieNode().getChildren();
-
-      // logger.info("wordID: " + wordID + " Vocabulary.word(Math.abs(wordID)) "
-      // + Vocabulary.word(Math.abs(wordID)));
-
-      if (!isNonterminal(wordID)) {
-        throw new RuntimeException("Error : expexted nonterminal, but did not get it "
-            + "in matchAllEqualOrBothNonTerminalAndNotGoalOrOOV(DotNode dotNode, int wordID)");
-      }
 
       if (childrenTbl != null) {
         // get all the extensions, map to string, check for *, build regexp
@@ -76,8 +154,10 @@ public class NonterminalMatcherFactory {
           Integer arcID = entry.getKey();
 
           String wordIdWord = Vocabulary.word(wordID);
-          if (isNonterminal(arcID) && !isOOVLabelOrGoalLabel(wordIdWord)) {
+
+          if (isNonterminal(arcID) && !isOOVLabelOrGoalLabel(wordIdWord, joshuaConfiguration)) {
             Trie value = entry.getValue();
+
             // logger.info("Substituing : " + arcWord + " for " + wordIdWord);
             trieList.add(value);
           }
@@ -86,6 +166,26 @@ public class NonterminalMatcherFactory {
       }
       // logger.info("trieList.size(): " + trieList.size());
       return trieList;
+
+    }
+
+    protected List<Trie> matchAllEqualOrBothNonTerminalAndNotGoalOrOOV(DotNode dotNode, int wordID) {
+      HashMap<Integer, ? extends Trie> childrenTbl = dotNode.getTrieNode().getChildren();
+
+      // logger.info("wordID: " + wordID + " Vocabulary.word(Math.abs(wordID)) "
+      // + Vocabulary.word(Math.abs(wordID)));
+
+      if (!isNonterminal(wordID)) {
+        throw new RuntimeException("Error : expexted nonterminal, but did not get it "
+            + "in matchAllEqualOrBothNonTerminalAndNotGoalOrOOV(DotNode dotNode, int wordID)");
+      }
+
+      if (useTargetQueryingToCollectAlternateNonterminals) {
+        return getNonTerminalsListFromChildrenByTargetedQuerying(childrenTbl);
+      } else {
+        return getNonTerminalsListFromChildrenByMatching(childrenTbl, wordID);
+      }
+
     }
   }
 
@@ -98,16 +198,22 @@ public class NonterminalMatcherFactory {
 
   protected abstract static class StandardNonterminalMatcher extends AbstractNonterminalMatcher {
 
-    protected StandardNonterminalMatcher(Logger logger, JoshuaConfiguration joshuaConfiguration) {
-      super(logger, joshuaConfiguration);
+    protected StandardNonterminalMatcher(Logger logger, JoshuaConfiguration joshuaConfiguration,
+        List<Integer> nonterminalIndicesExceptForGoalAndOOV,
+        boolean useTargetQueryingToCollectAlternateNonterminals) {
+      super(logger, joshuaConfiguration, nonterminalIndicesExceptForGoalAndOOV,
+          useTargetQueryingToCollectAlternateNonterminals);
     }
   }
 
   protected static class StandardNonterminalMatcherStrict extends StandardNonterminalMatcher {
 
     protected StandardNonterminalMatcherStrict(Logger logger,
-        JoshuaConfiguration joshuaConfiguration) {
-      super(logger, joshuaConfiguration);
+        JoshuaConfiguration joshuaConfiguration,
+        List<Integer> nonterminalIndicesExceptForGoalAndOOV,
+        boolean useTargetQueryingToCollectAlternateNonterminals) {
+      super(logger, joshuaConfiguration, nonterminalIndicesExceptForGoalAndOOV,
+          useTargetQueryingToCollectAlternateNonterminals);
     }
 
     @Override
@@ -126,8 +232,11 @@ public class NonterminalMatcherFactory {
      * @param joshuaConfiguration
      */
     protected StandardNonterminalMatcherSoftConstraints(Logger logger,
-        JoshuaConfiguration joshuaConfiguration) {
-      super(logger, joshuaConfiguration);
+        JoshuaConfiguration joshuaConfiguration,
+        List<Integer> nonterminalIndicesExceptForGoalAndOOV,
+        boolean useTargetQueryingToCollectAlternateNonterminals) {
+      super(logger, joshuaConfiguration, nonterminalIndicesExceptForGoalAndOOV,
+          useTargetQueryingToCollectAlternateNonterminals);
     }
 
     /**
@@ -142,7 +251,7 @@ public class NonterminalMatcherFactory {
         SuperNode superNode) {
 
       // We do not allow substitution of other things for GOAL labels or OOV symbols
-      if (isOOVLabelOrGoalLabel(Vocabulary.word(superNode.lhs))) {
+      if (isOOVLabelOrGoalLabel(Vocabulary.word(superNode.lhs), joshuaConfiguration)) {
         // logger.info("BLAA - Vocabulary.word(superNode.lhs)" + Vocabulary.word(superNode.lhs));
         Trie child_node = dotNode.getTrieNode().match(superNode.lhs);
         // logger.info("child_node.toString()" + child_node);
