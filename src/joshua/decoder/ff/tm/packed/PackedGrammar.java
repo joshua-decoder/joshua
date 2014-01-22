881ebe26012d5938a8d85cf489d03caf714005dd
Matt Post <post@cs.jhu.edu>
2014-01-22 16:43:39 -0500

    Bugfix: shadow declaration in catch

--- src/joshua/decoder/ff/tm/packed/PackedGrammar.java
+++ src/joshua/decoder/ff/tm/packed/PackedGrammar.java
@@ -332,13 +332,13 @@ public class PackedGrammar extends BatchGrammar {
         int feature_id = encoding.readId(features, feature_position);
         FloatEncoder encoder = encoding.encoder(feature_id);
 
+        String feature_name = Vocabulary.word(encoding.outerId(feature_id));
         try {
-          String name = Vocabulary.word(encoding.outerId(feature_id));
-          int index = Integer.parseInt(name);
+          int index = Integer.parseInt(feature_name);
           sb.append(String.format(" tm_%s_%d=%.5f", Vocabulary.word(owner), index,
               encoder.read(features, feature_position)));
         } catch (NumberFormatException e) {
-          sb.append(String.format(" %s=%.5f", name, encoder.read(features, feature_position)));
+          sb.append(String.format(" %s=%.5f", feature_name, encoder.read(features, feature_position)));
         }
 
         feature_position += EncoderConfiguration.ID_SIZE + encoder.size();
