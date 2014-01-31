4064c346dde5889d7f26bf843ce2d66c12c2f78f
Matt Post <post@cs.jhu.edu>
2014-01-31 11:39:41 -0500

    Bugfixes to pipeline relating to tune and test grammars

--- scripts/training/pipeline.pl
+++ scripts/training/pipeline.pl
@@ -1190,7 +1190,7 @@ my $TUNE_GRAMMAR = (defined $TUNE_GRAMMAR_FILE)
 		: $GRAMMAR_FILE;
 
 if ($DO_FILTER_TM and ! $DOING_LATTICES and ! defined $TUNE_GRAMMAR_FILE) {
-  $TUNE_GRAMMAR = "$DATA_DIRS{tune}/grammar.filtered.gz";
+  $TUNE_GRAMMAR = $TUNE_GRAMMAR_FILE = "$DATA_DIRS{tune}/grammar.filtered.gz";
 
   $cachepipe->cmd("filter-tune",
 									"$CAT $GRAMMAR_FILE | java -Xmx2g -Dfile.encoding=utf8 -cp $JOSHUA/class joshua.tools.TestSetFilter $FILTERING -v $TUNE{source} | $SCRIPTDIR/training/filter-rules.pl -bus$SCOPE | gzip -9n > $TUNE_GRAMMAR",
@@ -1419,7 +1419,7 @@ for my $run (1..$OPTIMIZER_RUNS) {
     $TEST_GRAMMAR = $TEST_GRAMMAR_FILE;
   } else {
     # otherwise, use the main grammar, and filter it if requested
-    $TEST_GRAMMAR = $GRAMMAR_FILE;
+    $TEST_GRAMMAR = $TEST_GRAMMAR_FILE = $GRAMMAR_FILE;
     
     if ($DO_FILTER_TM and ! $DOING_LATTICES) {
       $TEST_GRAMMAR = "$DATA_DIRS{test}/grammar.filtered.gz";
@@ -1621,7 +1621,7 @@ if ($TEST_GRAMMAR_FILE) {
   $TEST_GRAMMAR = $TEST_GRAMMAR_FILE;
 } else {
   # otherwise, use the main grammar, and filter it if requested
-  $TEST_GRAMMAR = $GRAMMAR_FILE;
+  $TEST_GRAMMAR = $TEST_GRAMMAR_FILE = $GRAMMAR_FILE;
   
   if ($DO_FILTER_TM and ! $DOING_LATTICES) {
 		$TEST_GRAMMAR = "$DATA_DIRS{test}/grammar.filtered.gz";
