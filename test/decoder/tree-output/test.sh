8aefaeedec83f12fab9168d87f29285e323adc77
Jonny Weese <jonny@cs.jhu.edu>
2014-12-23 14:10:46 -0500

    tests for tree format output

---
+++ test/decoder/tree-output/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -u
+
+cat input | $JOSHUA/bin/joshua-decoder -m 1g -threads 1 -c joshua.config > output 2> log
+
+# Compare
+diff -u output output.gold > diff
+
+if [ $? -eq 0 ]; then
+	rm -f diff log output output.scores
+	exit 0
+else
+	exit 1
+fi
