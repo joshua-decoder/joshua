c96250933e4f4ab9919704f9c8d7436e05bf2c3e
Matt Post <post@cs.jhu.edu>
2015-01-06 15:20:18 -0500

    Fixed test cases

--- test/decoder/num_translation_options/test.sh
+++ test/decoder/num_translation_options/test.sh
@@ -2,7 +2,9 @@
 
 set -u
 
-cat input | $JOSHUA/bin/joshua-decoder -m 1g -threads 1 -c joshua.config > output 2> log
+cat input | $JOSHUA/bin/joshua-decoder -c joshua.config > output 2> log
+cat input | $JOSHUA/bin/joshua-decoder -c joshua.config -no-dot-chart >> output 2>> log
+cat input | $JOSHUA/bin/joshua-decoder -c joshua.config.packed >> output 2>> log
 
 # Compare
 diff -u output output.gold > diff
