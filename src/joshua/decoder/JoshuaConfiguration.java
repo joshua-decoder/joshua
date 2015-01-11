2064fc37bf5f7d3941e29f7f602e295bc91ba7a9
keisks <keisks5@gmail.com>
2015-01-10 20:48:16 -0500

    restore java.io and added Buffer|FileReader explicitly

--- src/joshua/decoder/JoshuaConfiguration.java
+++ src/joshua/decoder/JoshuaConfiguration.java
@@ -1,6 +1,11 @@
 package joshua.decoder;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.logging.Logger;
