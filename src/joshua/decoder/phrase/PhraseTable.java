80d8d55bf0068506f5a2bf4e50bb20768d6abb92
Matt Post <post@cs.jhu.edu>
2015-02-26 23:09:38 -0500

    bugfix: don't use default_non_terminal, instead hardcode "[X]"

--- src/joshua/decoder/phrase/PhraseTable.java
+++ src/joshua/decoder/phrase/PhraseTable.java
@@ -99,7 +99,7 @@ public class PhraseTable implements Grammar {
         ? Vocabulary.id(Vocabulary.word(sourceWord) + "_OOV")
         : sourceWord;   
 
-    int nt_i = Vocabulary.id(this.config.default_non_terminal);
+    int nt_i = Vocabulary.id("[X]");
     Rule oovRule = new Rule(nt_i, new int[] { nt_i, sourceWord },
         new int[] { -1, targetWord }, "", 1, null);
     addRule(oovRule);
