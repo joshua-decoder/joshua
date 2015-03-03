e4c1f4968324db9915041b90029544d360df0d78
Matt Post <post@cs.jhu.edu>
2015-03-03 17:38:50 -0500

    Added missing file

---
+++ scripts/support/moses_phrase_to_joshua.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# Converts a Moses phrase table to a Joshua grammar, suitable for packing.
+# (Joshua can read in Moses phrase tables directly when using the in-memory
+# representation, so in that case there is no need to do the conversion).
+
+use strict;
+use warnings;
+
+while (my $line = <>) {
+  my @tokens = split(/ \|\|\| /, $line);
+
+  unshift(@tokens, "[X]");
+  $tokens[1] = "[X,1] $tokens[1]";
+  $tokens[2] = "[X,1] $tokens[2]";
+  $tokens[3] = join(" ", map { -log($_) } split(' ', $tokens[3]));
+
+  print join(" ||| ", @tokens);
+}
